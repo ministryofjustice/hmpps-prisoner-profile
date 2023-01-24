6863a3e33958bf950b80b609a340fb95f80f2a11
David Middleton <david.middleton@digital.justice.gov.uk>
2023-01-24 17:32:44 +0000

    ALIGN-40 Actions Block Create actions block macro (#40)

--- server/routes/index.test.ts
+++ server/routes/index.test.ts
@@ -100,4 +100,15 @@ describe('GET /', () => {
         })
       })
   })
+
+  it('should render actions block', () => {
+    return request(app)
+      .get('/prisoner/111222')
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('<div class="hmpps-actions-block">')
+        expect(res.text).toContain('<h2 class="govuk-heading-m">Actions</h2>')
+        expect(res.text).toContain('<ul class="hmpps-actions-block__list">')
+      })
+  })
 })
