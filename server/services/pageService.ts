42a57f7f6a4820ce94e9d8286b58ae99d25c45ef
Dan Burnley <dan@danops.uk>
2023-02-08 13:02:36 +0000

    Pass token instead of token store to prisoner search client

--- server/services/pageService.ts
+++ server/services/pageService.ts
@@ -1,14 +1,12 @@
 import { Response } from 'express'
 import { Prisoner } from '../interfaces/prisoner'
 import PrisonerSearchClient from '../data/prisonerSearchClient'
-import TokenStore from '../data/tokenStore'
-import { createRedisClient } from '../data/redisClient'
 import { mapHeaderData } from '../mappers/headerMappers'
 
 export default class PageService {
   renderPage<T>(_res: Response, prisonerNumber: string, template: string, pageData: T) {
-    const services = new PrisonerSearchClient(new TokenStore(createRedisClient({ legacyMode: false })))
-    services.getPrisonerDetails(_res.locals.user.token, prisonerNumber).then((prisonerData: Prisoner) => {
+    const services = new PrisonerSearchClient(_res.locals.user.token)
+    services.getPrisonerDetails(prisonerNumber).then((prisonerData: Prisoner) => {
       _res.render(template, {
         ...mapHeaderData(prisonerData),
         ...pageData,
