2ea12572baf9c556fdc89be9665c9d531e579ea4
Dan Burnley <dan@danops.uk>
2023-02-17 11:50:41 +0000

    Refactor integration tests

--- integration_tests/mockApis/prison.ts
+++ integration_tests/mockApis/prison.ts
@@ -6,139 +6,8 @@ import {
   visitBalancesMock,
   visitSummaryMock,
 } from '../../server/data/localMockData/miniSummaryMock'
-
-const nonAssociationA = {
-  offenderNo: 'ABC123',
-  firstName: 'John',
-  lastName: 'Doe',
-  reasonCode: 'VIC',
-  reasonDescription: 'Victim',
-  agencyDescription: 'Moorland (HMP & YOI)',
-  assignedLivingUnitDescription: 'NMI-RECP',
-  assignedLivingUnitId: 1234,
-}
-
-const nonAssociationDetailA = {
-  reasonCode: 'VIC',
-  reasonDescription: 'Victim',
-  typeCode: 'LAND',
-  typeDescription: 'Do Not Locate on Same Landing',
-  effectiveDate: '1980-01-01T00:00:00',
-  offenderNonAssociation: nonAssociationA,
-  expiryDate: '2020-07-17T00:00:00',
-  comments: 'Gang violence',
-  authorisedBy: 'Someone',
-}
-
-const nonAssociationB = {
-  offenderNo: 'DEF321',
-  firstName: 'Guy',
-  lastName: 'Incognito',
-  reasonCode: 'RIV',
-  reasonDescription: 'Rival Gang',
-  agencyDescription: 'Moorland (HMP & YOI)',
-  assignedLivingUnitDescription: 'NMI-RECP',
-  assignedLivingUnitId: 1234,
-}
-
-const nonAssociationDetailB = {
-  reasonCode: 'VIC',
-  reasonDescription: 'Victim',
-  typeCode: 'LAND',
-  typeDescription: 'Do Not Locate on Same Landing',
-  effectiveDate: '2020-07-01T00:00:00',
-  expiryDate: '2020-07-17T00:00:00',
-  comments: 'hjkhjkhjkhkhkj hjkhjkhjkhjkhjk',
-  authorisedBy: 'Jane Doe',
-  offenderNonAssociation: nonAssociationB,
-}
-
-const nonAssociationDetailsDummyData = {
-  offenderNo: 'G6123VU',
-  firstName: 'John',
-  lastName: 'Saunders',
-  agencyDescription: 'Moorland (HMP & YOI)',
-  assignedLivingUnitDescription: 'MDI-5-1-A-012',
-  assignedLivingUnitId: 26019,
-  nonAssociations: [nonAssociationDetailA, nonAssociationDetailB],
-}
-
-const scheduledEventsMock = [
-  {
-    bookingId: 123456,
-    eventClass: 'INT_MOV',
-    eventId: 54321,
-    eventStatus: 'SCH',
-    eventType: 'PRISON_ACT',
-    eventTypeDesc: 'Prison Activities',
-    eventSubType: 'PA',
-    eventSubTypeDesc: 'Prison Activities',
-    eventDate: '2023-02-14',
-    startTime: '2023-02-14T08:30:00',
-    endTime: '2023-02-14T11:45:00',
-    eventLocation: 'WORKSHOP 16 - JOINERY',
-    eventLocationId: 27026,
-    eventSource: 'PA',
-    eventSourceCode: 'JOINERY_AM',
-    eventSourceDesc: 'Joinery AM',
-    paid: false,
-    payRate: 1.05,
-    locationCode: 'WS16',
-  },
-  {
-    bookingId: 123456,
-    eventClass: 'INT_MOV',
-    eventId: 54321,
-    eventStatus: 'SCH',
-    eventType: 'PRISON_ACT',
-    eventTypeDesc: 'Prison Activities',
-    eventSubType: 'PA',
-    eventSubTypeDesc: 'Prison Activities',
-    eventDate: '2023-02-14',
-    startTime: '2023-02-14T13:15:00',
-    endTime: '2023-02-14T16:15:00',
-    eventLocation: 'WORKSHOP 16 - JOINERY',
-    eventLocationId: 27026,
-    eventSource: 'PA',
-    eventSourceCode: 'JOINERY_PM',
-    eventSourceDesc: 'Joinery PM',
-    paid: false,
-    payRate: 1.05,
-    locationCode: 'WS16',
-  },
-  {
-    bookingId: 123456,
-    eventClass: 'INT_MOV',
-    eventStatus: 'SCH',
-    eventType: 'APP',
-    eventTypeDesc: 'Appointment',
-    eventSubType: 'GYMF',
-    eventSubTypeDesc: 'Gym - Football',
-    eventDate: '2023-02-14',
-    startTime: '2023-02-14T18:00:00',
-    endTime: '2023-02-14T19:00:00',
-    eventLocation: 'GYM',
-    eventSource: 'APP',
-    eventSourceCode: 'APP',
-    eventSourceDesc: 'test',
-  },
-  {
-    bookingId: 123456,
-    eventClass: 'INT_MOV',
-    eventStatus: 'SCH',
-    eventType: 'APP',
-    eventTypeDesc: 'Appointment',
-    eventSubType: 'VLB',
-    eventSubTypeDesc: 'VLB - Test',
-    eventDate: '2023-02-14',
-    startTime: '2023-02-14T18:00:00',
-    endTime: '2023-02-14T19:00:00',
-    eventLocation: 'OFFICIAL VISITS',
-    eventSource: 'APP',
-    eventSourceCode: 'APP',
-    eventSourceDesc: 'test',
-  },
-]
+import dummyScheduledEvents from '../../server/data/localMockData/eventsForToday'
+import nonAssociationsDummyData from '../../server/data/localMockData/nonAssociations'
 
 export default {
   stubNonAssociations: (prisonerNumber: string) => {
@@ -152,7 +21,7 @@ export default {
         headers: {
           'Content-Type': 'application/json;charset=UTF-8',
         },
-        jsonBody: nonAssociationDetailsDummyData,
+        jsonBody: nonAssociationsDummyData,
       },
     })
   },
@@ -242,7 +111,7 @@ export default {
         headers: {
           'Content-Type': 'application/json;charset=UTF-8',
         },
-        jsonBody: scheduledEventsMock,
+        jsonBody: dummyScheduledEvents,
       },
     })
   },
