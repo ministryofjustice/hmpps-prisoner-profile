a66668db0202fe5a5fb1e313a9c47981523bbd96
David Middleton <david.middleton@digital.justice.gov.uk>
2023-04-06 11:19:57 +0100

    Test updates

--- integration_tests/e2e/alertsPage.cy.ts
+++ integration_tests/e2e/alertsPage.cy.ts
@@ -119,6 +119,7 @@ context('Alerts Page', () => {
     it('Moves to page 2 when clicking Next and back to page 1 when clicking Previous', () => {
       alertsPage.paginationCurrentPage().contains('1')
       alertsPage.paginationNextLink().first().click()
+      cy.url().should('include', 'page=2')
       alertsPage.paginationCurrentPage().contains('2')
       alertsPage.paginationSummaryHeader().contains('Showing 21 to 40 of 80 alerts')
 
