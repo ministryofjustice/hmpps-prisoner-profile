3b9029b87f51f163453bab30e047d3d69bd3e4e0
David Middleton <david.middleton@digital.justice.gov.uk>
2023-04-18 14:46:27 +0100

    Datepicker refactored

--- assets/js/hmppsDatepickerInit.js
+++
@@ -1,8 +0,0 @@
-/**
- * Get any elements with data-module 'ds-datepicker' and attach date picker
- */
-import DSDatePicker from './js/hmppsDatepicker.js'
-
-Array.from(document.querySelectorAll('[data-module="ds-datepicker"]')).forEach(element => {
-  new DSDatePicker(element).init()
-})
