ba9e6ebf5be42c06957db5f2775ef1dc4b0e53f8
Jon Brighton <brightonsbox@hotmail.com>
2023-06-09 11:31:32 +0100

    CDPS-199: No longer caching user caseloads

--- server/middleware/populateCurrentUser.ts
+++ server/middleware/populateCurrentUser.ts
@@ -3,8 +3,9 @@ import jwtDecode from 'jwt-decode'
 import logger from '../../logger'
 import UserService from '../services/userService'
 import config from '../config'
+import { CaseLoad } from '../interfaces/caseLoad'
 
-export default function populateCurrentUser(userService: UserService): RequestHandler {
+export function populateCurrentUser(userService: UserService): RequestHandler {
   return async (req, res, next) => {
     try {
       if (!req.session.userDetails) {
@@ -27,6 +28,29 @@ export default function populateCurrentUser(userService: UserService): RequestHa
       next()
     } catch (error) {
       logger.error(error, `Failed to retrieve user for: ${res.locals.user && res.locals.user.username}`)
+      next(error)
+    }
+  }
+}
+
+export function getUserCaseLoads(userService: UserService): RequestHandler {
+  return async (req, res, next) => {
+    try {
+      if (res.locals.user) {
+        const userCaseLoads = res.locals.user && (await userService.getUserCaseLoads(res.locals.user.token))
+        if (userCaseLoads && Array.isArray(userCaseLoads)) {
+          const availableCaseLoads = userCaseLoads.filter(caseload => caseload.type !== 'APP')
+          const activeCaseLoad = availableCaseLoads.filter((caseLoad: CaseLoad) => caseLoad.currentlyActive)[0]
+
+          res.locals.user.caseLoads = availableCaseLoads
+          res.locals.user.activeCaseLoad = activeCaseLoad
+        } else {
+          logger.info('No user case loads available')
+        }
+      }
+      next()
+    } catch (error) {
+      logger.error(error, `Failed to retrieve case loads for: ${res.locals.user && res.locals.user.username}`)
       next(error)
     }
   }
--- server/services/userService.test.ts
+++ server/services/userService.test.ts
@@ -3,7 +3,6 @@ import HmppsAuthClient, { User } from '../data/hmppsAuthClient'
 
 import PrisonApiClient from '../data/prisonApiClient'
 import { prisonApiClientMock } from '../../tests/mocks/prisonApiClientMock'
-import { CaseLoadsDummyDataA } from '../data/localMockData/caseLoad'
 import { LocationDummyDataB } from '../data/localMockData/locations'
 
 const token = 'some token'
@@ -32,13 +31,6 @@ describe('User service', () => {
     it('Retrieves and formats user name', async () => {
       const result = await userService.getUser(token)
       expect(result.displayName).toEqual('John Smith')
-    })
-
-    it('Retrieves user caseloads', async () => {
-      prisonApiClient.getUserCaseLoads = jest.fn(async () => CaseLoadsDummyDataA)
-
-      const result = await userService.getUser(token)
-      expect(result.caseLoads).toEqual(CaseLoadsDummyDataA)
     })
 
     it('Retrieves user locations', async () => {
