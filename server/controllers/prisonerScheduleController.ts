d0d7acccdff54f1e9b2ac46d4c8b15eb61d54fa7
James Grant <james.grant@digital.justice.gov.uk>
2023-09-07 19:51:11 +0100

    CDPS-31-update linting

--- server/controllers/prisonerScheduleController.ts
+++ server/controllers/prisonerScheduleController.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express";
-import { RestClientBuilder } from "../data";
-import { PrisonApiClient } from "../data/interfaces/prisonApiClient";
-import { Prisoner } from "../interfaces/prisoner";
-import { mapHeaderNoBannerData } from "../mappers/headerMappers";
-import { formatName, groupBy, times } from "../utils/utils";
-import { add, format, getHours, isAfter, isBefore, startOfToday } from "date-fns";
-import { formatDate } from "../utils/dateHelpers";
-import { GetEventScheduleItem } from "../interfaces/prisonApi/getEventScheduleItem";
-import { NameFormatStyle } from "../data/enums/nameFormatStyle";
+import { Request, Response } from 'express'
+import { add, format, getHours, isAfter, isBefore, startOfToday } from 'date-fns'
+import { RestClientBuilder } from '../data'
+import { PrisonApiClient } from '../data/interfaces/prisonApiClient'
+import { Prisoner } from '../interfaces/prisoner'
+import { mapHeaderNoBannerData } from '../mappers/headerMappers'
+import { formatName, groupBy, times } from '../utils/utils'
+import { formatDate } from '../utils/dateHelpers'
+import { GetEventScheduleItem } from '../interfaces/prisonApi/getEventScheduleItem'
+import { NameFormatStyle } from '../data/enums/nameFormatStyle'
 
 /**
  * Parse requests for case notes routes and orchestrate response
@@ -15,10 +15,10 @@ import { NameFormatStyle } from "../data/enums/nameFormatStyle";
 
 export default class PrisonerScheduleController {
   private prisonApiClient: PrisonApiClient
+
   constructor(private readonly prisonApiClientBuilder: RestClientBuilder<PrisonApiClient>) {}
 
-  public async displayPrisonerSchedule(req: Request, res: Response, prisonerData: Prisoner)  {
-
+  public async displayPrisonerSchedule(req: Request, res: Response, prisonerData: Prisoner) {
     const { firstName, middleNames, lastName } = prisonerData
     const name = formatName(firstName, middleNames, lastName, { style: NameFormatStyle.firstLast })
 
@@ -37,57 +37,51 @@ export default class PrisonerScheduleController {
     }
 
     const groupedByDate = groupBy(schedule, 'eventDate')
-    const todaysDate = format(startOfToday(), 'yyyy-MM-dd')
     const oneWeekToday = format(add(startOfToday(), { weeks: 1 }), 'yyyy-MM-dd')
-    const startOfWeek = when === 'nextWeek' ? oneWeekToday : todaysDate
-    const selectedWeekDates: {date: string}[] = []
+    const selectedWeekDates: { date: string }[] = []
 
-    if(when === 'nextWeek'){
+    if (when === 'nextWeek') {
       times(7)((i: number) =>
         selectedWeekDates.push({
-          date: format(add(startOfToday(), { weeks: 1, days: i }), 'yyyy-MM-dd')
-        })
+          date: format(add(startOfToday(), { weeks: 1, days: i }), 'yyyy-MM-dd'),
+        }),
       )
     } else {
       times(7)((i: number) =>
         selectedWeekDates.push({
-          date: format(add(startOfToday(), { days: i }), 'yyyy-MM-dd')
-        })
+          date: format(add(startOfToday(), { days: i }), 'yyyy-MM-dd'),
+        }),
       )
     }
 
     const filterMorning = (activities: GetEventScheduleItem[]) =>
-      activities && activities.filter((activity) => getHours(parseInt(activity.startTime)) < 12)
+      activities && activities.filter(activity => getHours(parseInt(activity.startTime, 10)) < 12)
 
     const filterAfternoon = (activities: GetEventScheduleItem[]) =>
-      activities &&
-      activities.filter(
-        (activity) => getHours(parseInt(activity.startTime)) < 17)
+      activities && activities.filter(activity => getHours(parseInt(activity.startTime, 10)) < 17)
 
     const filterEveningDuties = (activities: GetEventScheduleItem[]) =>
-        activities && activities.filter((activity) =>  getHours(parseInt(activity.startTime)) >= 17)
+      activities && activities.filter(activity => getHours(parseInt(activity.startTime, 10)) >= 17)
 
     const byStartTimeThenByEndTime = (a: GetEventScheduleItem, b: GetEventScheduleItem) => {
-      isBefore(parseInt(a.startTime), parseInt(b.startTime))
-      if (isBefore(parseInt(a.startTime), parseInt(b.startTime))) return -1
-    
-      if (isAfter(parseInt(a.startTime), parseInt(b.startTime))) return 1
-    
+      isBefore(parseInt(a.startTime, 10), parseInt(b.startTime, 10))
+      if (isBefore(parseInt(a.startTime, 10), parseInt(b.startTime, 10))) return -1
+
+      if (isAfter(parseInt(a.startTime, 10), parseInt(b.startTime, 10))) return 1
+
       if (!a.endTime) return -1
       if (!b.endTime) return 1
-    
-      if (isBefore(parseInt(a.endTime), parseInt(b.endTime))) return -1
-      if (isAfter(parseInt(a.endTime), parseInt(b.endTime))) return 1
-    
+
+      if (isBefore(parseInt(a.endTime, 10), parseInt(b.endTime, 10))) return -1
+      if (isAfter(parseInt(a.endTime, 10), parseInt(b.endTime, 10))) return 1
+
       return 0
     }
-    
-    
-    function formatEvent(eventSchedule: GetEventScheduleItem){
 
+    function formatEvent(eventSchedule: GetEventScheduleItem) {
       const { startTime, endTime, eventStatus, eventSubType, eventSubTypeDesc, eventSourceDesc } = eventSchedule
       const comment = eventSubType === 'PA' ? null : eventSourceDesc
-    
+
       return {
         comment,
         startTime,
@@ -99,22 +93,22 @@ export default class PrisonerScheduleController {
       }
     }
 
-    function eventsAction(events:  GetEventScheduleItem[]){
+    function eventsAction(events: GetEventScheduleItem[]) {
       const morningActivity = filterMorning(events)
       const afternoonActivity = filterAfternoon(events)
       const eveningDuties = filterEveningDuties(events)
 
       return {
         morningActivities:
-          morningActivity && morningActivity.map((activity) => formatEvent(activity)).sort(byStartTimeThenByEndTime),
+          morningActivity && morningActivity.map(activity => formatEvent(activity)).sort(byStartTimeThenByEndTime),
         afternoonActivities:
-          afternoonActivity && afternoonActivity.map((activity) => formatEvent(activity)).sort(byStartTimeThenByEndTime),
+          afternoonActivity && afternoonActivity.map(activity => formatEvent(activity)).sort(byStartTimeThenByEndTime),
         eveningActivities:
-          eveningDuties && eveningDuties.map((activity) => formatEvent(activity)).sort(byStartTimeThenByEndTime),
+          eveningDuties && eveningDuties.map(activity => formatEvent(activity)).sort(byStartTimeThenByEndTime),
       }
     }
 
-    const days = selectedWeekDates.map((day) => ({
+    const days = selectedWeekDates.map(day => ({
       date: formatDate(day.date),
       periods: eventsAction(groupedByDate[day.date]),
     }))
@@ -122,9 +116,9 @@ export default class PrisonerScheduleController {
     return res.render('pages/prisonerSchedule', {
       pageTitle: 'Location details',
       ...mapHeaderNoBannerData(prisonerData),
-      days: days,
+      days,
       name,
-      nextWeekStartDate: oneWeekToday
+      nextWeekStartDate: oneWeekToday,
     })
   }
 }
