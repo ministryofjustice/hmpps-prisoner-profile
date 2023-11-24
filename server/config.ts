b79b9eb369591fd2a05354e0b0342d77a8aa8303
Matthew Whitfield <matthew.whitfield@digital.justice.gov.uk>
2023-11-24 16:45:07 +0000

    Revert "Fix typo in audit config (#375)"
    This reverts commit 1649b7cb3485ff20642f89f1e9962f65f4f24467.

--- server/config.ts
+++ server/config.ts
@@ -45,8 +45,8 @@ export default {
   },
   apis: {
     audit: {
-      queueUrl: get('AUDIT_SQS_QUEUE_URL', 'http://localhost:4566/000000000000/mainQueue', requiredInProduction),
-      region: get('AUDIT_SQS_REGION', 'eu-west-2', requiredInProduction),
+      queueUrl: get('AUDIT_QUEUE_URL', 'eu-west-2', requiredInProduction),
+      region: get('AUDIT_QUEUE_REGION', 'http://localhost:4566/000000000000/mainQueue', requiredInProduction),
       serviceName: get('AUDIT_SERVICE_NAME', 'hmpps-prisoner-profile', requiredInProduction),
       enabled: get('AUDIT_ENABLED', 'false') === 'true',
     },
