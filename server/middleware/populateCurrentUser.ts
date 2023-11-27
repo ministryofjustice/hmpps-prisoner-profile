0a62c8580a5871a61df02e7fb0761aa9658c724a
Daniel Burnley <dan@danops.uk>
2023-11-27 13:24:35 +0000

    CDPS-560: Pull active caseload ID from active caseload (#379)

--- server/middleware/populateCurrentUser.ts
+++ server/middleware/populateCurrentUser.ts
@@ -40,6 +40,7 @@ export function getUserCaseLoads(userService: UserService): RequestHandler {
 
           res.locals.user.caseLoads = availableCaseLoads
           res.locals.user.activeCaseLoad = activeCaseLoad
+          res.locals.user.activeCaseLoadId = activeCaseLoad.caseLoadId
         } else {
           logger.info('No user case loads available')
         }
