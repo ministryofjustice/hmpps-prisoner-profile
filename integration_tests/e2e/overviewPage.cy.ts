aaeaff485fb7fb9b4cfea2729d0a0e44275cfe1f
Daniel Burnley <dan@danops.uk>
2023-12-06 14:13:00 +0000

    CDPS-368: Pull ethnic group code onto the overview (#387)
    * CDPS-368: Pull ethnic group code onto the overview
    
    * Refactor personal data object on overview page

--- integration_tests/e2e/overviewPage.cy.ts
+++ integration_tests/e2e/overviewPage.cy.ts
@@ -163,7 +163,19 @@ context('Overview Page', () => {
     context('Personal details', () => {
       it('Displays the prisoner presonal details', () => {
         const overviewPage = Page.verifyOnPage(OverviewPage)
-        overviewPage.personalDetails().should('exist')
+        overviewPage.personalDetails().perferredName().should('include.text', 'John')
+        overviewPage.personalDetails().dateOfBirth().should('include.text', '12/10/1990')
+        overviewPage.personalDetails().age().should('include.text', '32')
+        overviewPage.personalDetails().nationality().should('include.text', 'Stateless')
+        overviewPage.personalDetails().spokenLanguage().should('include.text', 'Welsh')
+
+        overviewPage
+          .personalDetails()
+          .ethnicGroup()
+          .should('include.text', 'White: Eng./Welsh/Scot./N.Irish/British (W1)')
+        overviewPage.personalDetails().religionOrBelief().should('include.text', 'Celestial Church of God')
+        overviewPage.personalDetails().croNumber().should('include.text', '400862/08W')
+        overviewPage.personalDetails().pncNumber().should('include.text', '08/359381C')
       })
     })
 
--- server/interfaces/personalDetails.ts
+++ server/interfaces/personalDetails.ts
@@ -8,6 +8,17 @@ export type PersonalDetail = {
 }
 
 export type PersonalDetails = {
-  personalDetailsMain: PersonalDetail[]
-  personalDetailsSide: PersonalDetail[]
+  personalDetailsMain: {
+    preferredName: string
+    dateOfBirth: string
+    age: string
+    nationality: string
+    spokenLanguage: string
+  }
+  personalDetailsSide: {
+    ethnicGroup: string
+    religionOrBelief: string
+    croNumber: string
+    pncNumber: string
+  }
 }
