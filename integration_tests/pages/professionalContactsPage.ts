51ebcb4842ff201ee092beae4ee22fc85e8e1bdb
David Middleton <david.middleton@digital.justice.gov.uk>
2023-12-19 14:04:12 +0000

    CDPS-558: Update professional contacts

--- integration_tests/pages/professionalContactsPage.ts
+++ integration_tests/pages/professionalContactsPage.ts
@@ -5,32 +5,32 @@ export default class ProfessionalContactsPage extends Page {
 
   contacts = (): PageElement => cy.get('.hmpps-summary-card')
 
-  pomContact = () => ({
+  keyWorkerContact = () => ({
     panel: (): PageElement => cy.get('.hmpps-summary-card').first(),
+    header: (): PageElement => this.keyWorkerContact().panel().get('[data-qa="summary-header"]'),
+    name: (): PageElement => this.keyWorkerContact().panel().find('[data-qa="contact-name"]'),
+    contactDetails: (): PageElement => this.keyWorkerContact().panel().find('[data-qa="contact-details"]'),
+    address: (): PageElement => this.keyWorkerContact().panel().find('[data-qa="contact-address"]'),
+  })
+
+  pomContact = () => ({
+    panel: (): PageElement => cy.get('.hmpps-summary-card').eq(1),
     header: (): PageElement => this.pomContact().panel().get('[data-qa="summary-header"]'),
     name: (): PageElement => this.pomContact().panel().find('[data-qa="contact-name"]'),
   })
 
   coPomContact = () => ({
-    panel: (): PageElement => cy.get('.hmpps-summary-card').eq(1),
+    panel: (): PageElement => cy.get('.hmpps-summary-card').eq(2),
     header: (): PageElement => this.coPomContact().panel().get('div [data-qa="summary-header"]'),
     name: (): PageElement => this.coPomContact().panel().find('[data-qa="contact-name"]'),
   })
 
   comContact = () => ({
-    panel: (): PageElement => cy.get('.hmpps-summary-card').eq(2),
+    panel: (): PageElement => cy.get('.hmpps-summary-card').eq(3),
     header: (): PageElement => this.comContact().panel().get('[data-qa="summary-header"]'),
     name: (): PageElement => this.comContact().panel().find('[data-qa="contact-name"]'),
     contactDetails: (): PageElement => this.comContact().panel().find('[data-qa="contact-details"]'),
     address: (): PageElement => this.comContact().panel().find('[data-qa="contact-address"]'),
-  })
-
-  keyWorkerContact = () => ({
-    panel: (): PageElement => cy.get('.hmpps-summary-card').eq(3),
-    header: (): PageElement => this.keyWorkerContact().panel().get('[data-qa="summary-header"]'),
-    name: (): PageElement => this.keyWorkerContact().panel().find('[data-qa="contact-name"]'),
-    contactDetails: (): PageElement => this.keyWorkerContact().panel().find('[data-qa="contact-details"]'),
-    address: (): PageElement => this.keyWorkerContact().panel().find('[data-qa="contact-address"]'),
   })
 
   firstPrisonContact = () => ({
