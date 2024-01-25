fe2326cb171793386031f9a959fbad23f3aff8ac
Daniel Burnley <dan@danops.uk>
2024-01-25 14:48:42 +0000

    CDPS-509: CSRA details updates (#443)
    * CDPS-509: Update CSRA details page
    
    * CDPS-509: Update CSRA Tag macro
    
    ---------
    
    Co-authored-by: William Falconer <williamfalconeruk@users.noreply.github.com>

--- integration_tests/pages/crsaReviewPage.ts
+++ integration_tests/pages/crsaReviewPage.ts
@@ -5,15 +5,15 @@ export default class CsraReviewPage extends Page {
 
   csraValue = (): PageElement => cy.get(':nth-child(1) > .govuk-summary-list__value')
 
-  authorisedBy = (): PageElement => cy.get(':nth-child(2) > .govuk-summary-list__value')
+  authorisedBy = (): PageElement => cy.get(':nth-child(3) > .govuk-summary-list__value')
 
-  location = (): PageElement => cy.get(':nth-child(3) > .govuk-summary-list__value')
+  location = (): PageElement => cy.get(':nth-child(7) > .govuk-summary-list__value')
 
-  comments = (): PageElement => cy.get(':nth-child(4) > .govuk-summary-list__value')
+  comments = (): PageElement => cy.get(':nth-child(5) > .govuk-summary-list__value')
 
-  reviewedBy = (): PageElement => cy.get(':nth-child(5) > .govuk-summary-list__value')
+  reviewedBy = (): PageElement => cy.get(':nth-child(8) > .govuk-summary-list__value')
 
-  nextReviewDate = (): PageElement => cy.get(':nth-child(6) > .govuk-summary-list__value')
+  nextReviewDate = (): PageElement => cy.get(':nth-child(9) > .govuk-summary-list__value')
 
   reviewQuestions = (): PageElement => cy.get('[data-test="review-questions"]')
 }
