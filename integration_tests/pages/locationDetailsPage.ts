f79c3fec09f82df16dcc608d09bb6461384ec00d
Jon Brighton <brightonsbox@hotmail.com>
2024-02-14 10:38:14 +0000

    CDPS-373: Adding integration tests for the previous locations section (#490)

--- integration_tests/pages/locationDetailsPage.ts
+++ integration_tests/pages/locationDetailsPage.ts
@@ -1,5 +1,14 @@
 import Page, { PageElement } from './page'
 
+interface PreviousLocationDetails {
+  location: HTMLElement
+  movedIn: HTMLElement
+  movedInBy: HTMLElement
+  movedOut: HTMLElement
+  linkVisible: boolean
+  link: string
+}
+
 export default class LocationDetailsPage extends Page {
   h1 = (): PageElement => cy.get('h1')
 
@@ -7,8 +16,31 @@ export default class LocationDetailsPage extends Page {
 
   moveToReceptionBtn = (): PageElement => cy.getDataQa('move-to-reception-action-button')
 
-  currentInmate = (inmateOrder): PageElement =>
+  currentInmate = (inmateOrder: number): PageElement =>
     cy.get(`[data-qa="currently-sharing-with"] > p:nth-child(${inmateOrder})`)
 
-  locationHistoryLink = (): PageElement => cy.getDataQa('location-history-link')
+  currentLocationHistoryLink = (): PageElement => cy.getDataQa('current-location-history-link')
+
+  previousLocationAgency = (agencyOrder: number): PageElement =>
+    cy.get(`[data-qa="previous-locations"] > div:nth-child(${agencyOrder}) > h3`)
+
+  previousLocation = (
+    agencyOrder: number,
+    locationInAgencyOrder: number,
+    callback: (previousLocationDetails: PreviousLocationDetails) => void,
+  ): PageElement =>
+    cy
+      .get(
+        `[data-qa="previous-locations"] > div:nth-child(${agencyOrder}) > table > tbody > tr:nth-child(${locationInAgencyOrder}) > td`,
+      )
+      .then(row => {
+        callback({
+          location: row[0],
+          movedIn: row[1],
+          movedInBy: row[2],
+          movedOut: row[3],
+          linkVisible: row[4].hasChildNodes(),
+          link: row[4].firstElementChild?.getAttribute('href'),
+        })
+      })
 }
