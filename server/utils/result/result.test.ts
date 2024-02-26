bd5bac677b0d03ce23d86f764f5098529352f7b9
Jon Brighton <brightonsbox@hotmail.com>
2024-02-26 23:40:48 +0000

    CDPS-636: Introducing Result type

---
+++ server/utils/result/result.test.ts
@@ -0,0 +1,124 @@
+import { rejected, fulfilled, toResult, resultsOf, resultOf } from './result'
+
+const error = new Error('Some error!')
+
+describe('result', () => {
+  describe('isFulfilled', () => {
+    it('reports fulfilled', () => {
+      expect(fulfilled(1).isFulfilled()).toBeTruthy()
+    })
+
+    it('reports rejected', () => {
+      expect(rejected(error).isFulfilled()).toBeFalsy()
+    })
+  })
+
+  describe('handle', () => {
+    it('can handle a fulfilled result', () => {
+      expect(fulfilled(1).handle({ fulfilled: i => i + 1, rejected: _e => null })).toEqual(2)
+      expect(fulfilled('hello').handle({ fulfilled: s => `${s} there`, rejected: _e => null })).toEqual('hello there')
+      expect(
+        fulfilled({ some: 'object' }).handle({ fulfilled: o => ({ ...o, another: 'field' }), rejected: _e => null }),
+      ).toEqual({ some: 'object', another: 'field' })
+    })
+
+    it('can handle a rejected result', () => {
+      expect(rejected(error).handle({ fulfilled: value => value, rejected: e => ({ handled: e.message }) })).toEqual({
+        handled: error.message,
+      })
+    })
+  })
+
+  describe('getOrThrow', () => {
+    it.each([1, 'hello', { some: 'object' }])('can retrieve the value from a fulfilled result', value => {
+      expect(fulfilled(value).getOrThrow()).toEqual(value)
+    })
+
+    it('can throw an error from a rejected result', () => {
+      expect(rejected(error).getOrThrow).toThrow(error)
+    })
+  })
+
+  describe('getOrHandle', () => {
+    it.each([1, 'hello', { some: 'object' }])('can retrieve the value from a fulfilled result', value => {
+      expect(fulfilled(value).getOrHandle(e => e.message)).toEqual(value)
+    })
+
+    it('can handle an error from a rejected result', () => {
+      expect(rejected(error).getOrHandle(e => e.message)).toEqual(error.message)
+    })
+  })
+
+  describe('getOrNull', () => {
+    it.each([1, 'hello', { some: 'object' }])('can retrieve the value from a fulfilled result', value => {
+      expect(fulfilled(value).getOrNull()).toEqual(value)
+    })
+
+    it('returns null from a rejected result', () => {
+      expect(rejected(error).getOrNull()).toBeNull()
+    })
+  })
+
+  describe('toResult', () => {
+    it('converts a PromiseFulfilledResult', async () => {
+      const [promiseResult] = await Promise.allSettled([Promise.resolve(1)])
+      expect(toResult(promiseResult).getOrThrow()).toEqual(1)
+    })
+
+    it('converts a PromiseRejectedResult', async () => {
+      const [promiseResult] = await Promise.allSettled([Promise.reject(error)])
+      expect(toResult(promiseResult).getOrThrow).toThrow(error)
+    })
+  })
+
+  describe('resultsOf', () => {
+    it('returns results for an array of Promises', async () => {
+      const [a, b, c, d] = await resultsOf([
+        Promise.resolve(1),
+        Promise.resolve('hello'),
+        Promise.resolve({ some: 'object' }),
+        Promise.reject(error),
+      ])
+
+      expect(a.getOrThrow()).toEqual(1)
+      expect(b.getOrThrow()).toEqual('hello')
+      expect(c.getOrThrow()).toEqual({ some: 'object' })
+      expect(d.getOrThrow).toThrow(error)
+    })
+  })
+
+  describe('resultOf', () => {
+    const functionReturnsAString = () => Promise.resolve('hello')
+    const functionReturnsAnObject = (i: number, s: string) => Promise.resolve({ number: i, string: s })
+
+    it('wraps individual functions in a try catch to return fulfilled results', async () => {
+      // Can opt to only return Result objects for those functions we can handle, for example:
+
+      const [a, b, c, d] = await Promise.all([
+        Promise.resolve(1),
+        resultOf(functionReturnsAString)(),
+        resultOf(functionReturnsAnObject)(123, 'abc'),
+        Promise.resolve(1.23),
+      ])
+
+      expect(a).toEqual(1)
+      expect(b.getOrHandle(e => e.message)).toEqual('hello')
+      expect(c.getOrHandle(e => e.message)).toEqual({ number: 123, string: 'abc' })
+      expect(d).toEqual(1.23)
+    })
+
+    it('wraps individual functions in a try catch to safely return rejected results', async () => {
+      const [a, b, c, d] = await Promise.all([
+        Promise.resolve(1),
+        resultOf(() => Promise.reject(error))(),
+        resultOf((_i: number, _s: string) => Promise.reject(error))(123, 'abc'),
+        Promise.resolve(1.23),
+      ])
+
+      expect(a).toEqual(1)
+      expect(b.getOrThrow).toThrow(error)
+      expect(c.getOrThrow).toThrow(error)
+      expect(d).toEqual(1.23)
+    })
+  })
+})
---
+++ server/utils/result/result.ts
@@ -0,0 +1,85 @@
+/**
+ * `Result` extends the `PromiseSettledResult` type returned from `Promise.allSettled`,
+ * adding some functions that make accessing and handling the result a bit easier,
+ * avoiding the need for if else statements everywhere.
+ */
+export type Result<T, E extends Error = Error> = PromiseSettledResult<T> & {
+  isFulfilled: () => boolean
+  handle: <R1, R2>(handler: ResultHandler<T, E, R1, R2>) => R1 | R2
+  getOrThrow: () => T
+  getOrHandle: <R1>(handler: (e: E) => R1) => T | R1
+  getOrNull: () => T
+}
+
+/**
+ * `ResultHandler` allows two functions to be provided to handle both the success
+ * and error case.
+ */
+export interface ResultHandler<T, E extends Error, R1, R2> {
+  fulfilled(value: T): R1
+  rejected(e: E): R2
+}
+
+/**
+ * `toResult` converts a `PromiseSettledResult` to a `Result` object
+ */
+export const toResult = <T>(promiseResult: PromiseSettledResult<T>) => {
+  if (promiseResult.status === 'fulfilled') {
+    return fulfilled(promiseResult.value)
+  }
+
+  return rejected(promiseResult.reason) as Result<T>
+}
+
+/**
+ * `resultsOf` replaces `Promise.allSettled` returning the extended `Result` objects
+ * instead of `PromiseSettledResults`
+ */
+export const resultsOf = async <T extends readonly unknown[] | []>(
+  values: T,
+): Promise<{ -readonly [P in keyof T]: Result<Awaited<T[P]>> }> => {
+  return (await Promise.allSettled(values)).map(toResult) as {
+    -readonly [P in keyof T]: Result<Awaited<T[P]>>
+  }
+}
+
+/**
+ * `resultOf` wraps a function call in a try catch and returns a `Result`
+ */
+export const resultOf =
+  <T, A extends unknown[]>(fn: (...args: A) => Promise<T>) =>
+  async (...args: A): Promise<Result<T>> => {
+    try {
+      return fulfilled(await fn(...args))
+    } catch (e) {
+      return rejected(e)
+    }
+  }
+
+/**
+ * `success` takes a value and produces a 'successful' `Result`
+ */
+export const fulfilled = <T, E extends Error>(value: T): Result<T, E> => ({
+  status: 'fulfilled',
+  value,
+  isFulfilled: () => true,
+  handle: <R1, R2>(handler: ResultHandler<T, E, R1, R2>) => handler.fulfilled(value),
+  getOrThrow: () => value,
+  getOrHandle: () => value,
+  getOrNull: () => value,
+})
+
+/**
+ * `error` takes a value and produces an 'errored' `Result`
+ */
+export const rejected = <T, E extends Error>(error: E): Result<T, E> => ({
+  status: 'rejected',
+  reason: error,
+  isFulfilled: () => false,
+  handle: <R1, R2>(handler: ResultHandler<T, E, R1, R2>) => handler.rejected(error),
+  getOrThrow: () => {
+    throw error
+  },
+  getOrHandle: <R1>(handler: (e: E) => R1) => handler(error),
+  getOrNull: () => null,
+})
