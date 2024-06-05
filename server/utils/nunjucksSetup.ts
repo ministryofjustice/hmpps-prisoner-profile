fdd34f073a8ca8a33657826399fbc98928eb2d73
Matthew Whitfield <matthew.whitfield@digital.justice.gov.uk>
2024-06-05 11:42:33 +0100

    CDPS-450 implement @ministryofjustice/hmpps-connect-dps-components and remove old method from codebase

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -65,6 +65,7 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
       'node_modules/govuk-frontend/dist/components/',
       'node_modules/@ministryofjustice/frontend/',
       'node_modules/@ministryofjustice/frontend/moj/components/',
+      'node_modules/@ministryofjustice/hmpps-connect-dps-components/dist/assets/',
     ],
     {
       autoescape: true,
