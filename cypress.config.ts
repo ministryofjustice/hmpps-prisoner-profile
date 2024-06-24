c10fdfbcc60f2546154b43f2693504224e71c6f5
Dani Burnley <dan@danops.uk>
2024-06-24 12:35:03 +0100

    CDPS-774: Temporarily only run edit tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -72,7 +72,8 @@ export default defineConfig({
     },
     baseUrl: 'http://localhost:3007',
     excludeSpecPattern: '**/!(*.cy).ts',
-    specPattern: 'integration_tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    // specPattern: 'integration_tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'integration_tests/e2e/personal/edit/editHeight.cy.{js,jsx,ts,tsx}',
     supportFile: 'integration_tests/support/index.ts',
   },
 })
