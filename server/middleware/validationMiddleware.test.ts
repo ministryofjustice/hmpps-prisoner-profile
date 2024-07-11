67225b3db442239fa500420357f81837f7196db9
Dani Burnley <dan@danops.uk>
2024-07-11 12:51:03 +0100

    CDPS-853: Update middleware with support for redirecting

--- server/middleware/validationMiddleware.test.ts
+++ server/middleware/validationMiddleware.test.ts
@@ -4,19 +4,20 @@ import validationMiddleware, { Validator } from './validationMiddleware'
 
 describe('Validation middleware', () => {
   let req: Request = {} as Request
-  const res = {} as Response
+  let res = {} as Response
   const next = jest.fn()
   const error: HmppsError = { text: 'error message', href: 'error' }
   const errorTwo: HmppsError = { text: 'error message 2', href: 'error2' }
 
   beforeEach(() => {
     jest.resetAllMocks()
-    req = {} as Request
+    res = { redirect: jest.fn() } as any
+    req = { flash: jest.fn() } as any
   })
 
   it('should add errors to request object when any are present', async () => {
     const alwaysFailsValidator: Validator = () => [error]
-    const middleware = validationMiddleware(alwaysFailsValidator)
+    const middleware = validationMiddleware([alwaysFailsValidator])
     await middleware(req, res, next)
 
     expect(req.errors).toEqual([error])
@@ -25,7 +26,7 @@ describe('Validation middleware', () => {
 
   it('should add errors to request object if any are present and return a promise', async () => {
     const alwaysFailsValidator: Validator = () => Promise.resolve([error])
-    await validationMiddleware(alwaysFailsValidator)(req, res, next)
+    await validationMiddleware([alwaysFailsValidator])(req, res, next)
 
     expect(req.errors).toEqual([error])
     expect(next).toHaveBeenCalled()
@@ -33,7 +34,7 @@ describe('Validation middleware', () => {
 
   it('should not add any errors to request object when no errors are present', async () => {
     const neverFailsValidator: Validator = () => []
-    await validationMiddleware(neverFailsValidator)(req, res, next)
+    await validationMiddleware([neverFailsValidator])(req, res, next)
 
     expect(req.errors).toEqual(undefined)
     expect(next).toHaveBeenCalled()
@@ -41,7 +42,7 @@ describe('Validation middleware', () => {
 
   it('should receive a request body', async () => {
     const mockValidator: Validator = jest.fn().mockReturnValue([])
-    validationMiddleware(mockValidator)(req, res, next)
+    validationMiddleware([mockValidator])(req, res, next)
 
     expect(mockValidator).toHaveBeenCalledWith(req.body)
   })
@@ -50,9 +51,44 @@ describe('Validation middleware', () => {
     const firstValidator: Validator = () => [error]
     const secondValidator: Validator = () => [errorTwo]
 
-    await validationMiddleware(firstValidator, secondValidator)(req, res, next)
+    await validationMiddleware([firstValidator, secondValidator])(req, res, next)
 
     expect(req.errors).toEqual([error, errorTwo])
     expect(next).toHaveBeenCalled()
+  })
+
+  describe('With options', () => {
+    const alwaysFailsValidator: Validator = () => Promise.resolve([error])
+
+    it('Redirects to the given path when redirectOnFail is true', async () => {
+      await validationMiddleware([alwaysFailsValidator], { redirectOnError: true, redirectPath: 'path' })(
+        req,
+        res,
+        next,
+      )
+
+      expect(res.redirect).toHaveBeenCalledWith('path')
+    })
+
+    it('Puts the errors into the flash for the redirect', async () => {
+      await validationMiddleware([alwaysFailsValidator], { redirectOnError: true, redirectPath: 'path' })(
+        req,
+        res,
+        next,
+      )
+
+      expect(req.flash).toHaveBeenCalledWith('errors', [error])
+    })
+
+    it('Calls on error if provided', async () => {
+      const options = {
+        redirectOnError: true,
+        redirectPath: 'path',
+        onError: jest.fn(),
+      }
+
+      await validationMiddleware([alwaysFailsValidator], options)(req, res, next)
+      expect(options.onError).toHaveBeenCalled()
+    })
   })
 })
--- server/middleware/validationMiddleware.ts
+++ server/middleware/validationMiddleware.ts
@@ -1,12 +1,26 @@
-import { RequestHandler } from 'express'
+import { Request, RequestHandler } from 'express'
 import HmppsError from '../interfaces/HmppsError'
 
 export type Validator = (body: Record<string, string>) => HmppsError[] | Promise<HmppsError[]>
 
-export default function validationMiddleware(...validators: Validator[]): RequestHandler {
+export default function validationMiddleware(
+  validators: Validator[],
+  options: {
+    redirectOnError: boolean
+    redirectPath?: string
+    onError?: (req: Request) => void
+  } = { redirectOnError: false },
+): RequestHandler {
   return async (req, res, next) => {
     const validationResults = await Promise.all(validators.map(validator => validator(req.body)))
     const errors = validationResults.flat()
+
+    if (options.redirectOnError) {
+      if (options.onError) options.onError(req)
+      req.flash('errors', errors)
+      res.redirect(options.redirectPath)
+    }
+
     if (errors.length) {
       req.errors = errors
     }
