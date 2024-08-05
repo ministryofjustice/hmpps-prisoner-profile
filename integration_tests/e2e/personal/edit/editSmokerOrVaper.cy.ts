9f19dac1929e56bd6e7d566ef65d74906e7bde3a
Dani Burnley <dan@danops.uk>
2024-08-05 15:38:14 +0100

    Fix smoker or vaper E2E test (#696)

--- integration_tests/e2e/personal/edit/editSmokerOrVaper.cy.ts
+++ integration_tests/e2e/personal/edit/editSmokerOrVaper.cy.ts
@@ -34,7 +34,7 @@ context('Edit smoker or vaper', () => {
     editPageWithTitle: EditPage,
     editPageTitle: 'Does John Saunders smoke or vape?',
     successfulFlashMessage: 'Smoker or vaper updated',
-    validInputs: { radioInput: 'Yes' },
+    validInputs: { radioInputs: { radioField: 'Yes' } },
     redirectAnchor: 'personal-details',
   })
 })
