c6926043f532f7cff2327c579e564e067b4fc34f
David Middleton <122619525+davidatmoj@users.noreply.github.com>
2024-11-06 14:51:46 +0000

    CDPS-980: Fix for null diet/allergies (#748)

--- server/services/personalPageService.ts
+++ server/services/personalPageService.ts
@@ -235,16 +235,14 @@ export default class PersonalPageService {
       socialCareNeeded: getProfileInformationValue(ProfileInformationType.SocialCareNeeded, profileInformation),
       typeOfDiet: getProfileInformationValue(ProfileInformationType.TypesOfDiet, profileInformation) || 'Not entered',
       youthOffender: prisonerData.youthOffender ? 'Yes' : 'No',
-      medicalDietaryRequirements: prisonPerson
-        ? prisonPerson.health?.medicalDietaryRequirements?.value
-            .map(({ id, description }) => ({ id, description }))
-            .sort((a, b) => a.description.localeCompare(b.description))
-        : [],
-      foodAllergies: prisonPerson
-        ? prisonPerson.health?.foodAllergies?.value
-            .map(({ id, description }) => ({ id, description }))
-            .sort((a, b) => a.description.localeCompare(b.description))
-        : [],
+      medicalDietaryRequirements:
+        prisonPerson?.health?.medicalDietaryRequirements?.value
+          ?.map(({ id, description }) => ({ id, description }))
+          .sort((a, b) => a.description.localeCompare(b.description)) ?? [],
+      foodAllergies:
+        prisonPerson?.health?.foodAllergies?.value
+          ?.map(({ id, description }) => ({ id, description }))
+          .sort((a, b) => a.description.localeCompare(b.description)) ?? [],
     }
   }
 
