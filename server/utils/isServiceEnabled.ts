079ee5d8a4016f9f97b36a7ea6c9573ec7d75fd6
Jon Brighton <brightonsbox@hotmail.com>
2024-12-05 10:52:01 +0000

    CDPS-1009: Using new case load middleware

--- server/utils/isServiceEnabled.ts
+++ server/utils/isServiceEnabled.ts
@@ -1,13 +1,13 @@
-import HeaderFooterMeta from '@ministryofjustice/hmpps-connect-dps-components/dist/types/HeaderFooterMeta'
+import HeaderFooterSharedData from '@ministryofjustice/hmpps-connect-dps-components/dist/types/HeaderFooterSharedData'
 
 export const isServiceEnabledForNavigation = (
   serviceId: string,
-  componentsMeta: HeaderFooterMeta | undefined,
+  componentsMeta: HeaderFooterSharedData | undefined,
 ): boolean => {
   return !!componentsMeta?.services?.find(service => service.id === serviceId && service.navEnabled)
 }
 
-export const isServiceEnabled = (serviceId: string, componentsMeta: HeaderFooterMeta | undefined): boolean => {
+export const isServiceEnabled = (serviceId: string, componentsMeta: HeaderFooterSharedData | undefined): boolean => {
   return !!componentsMeta?.services?.find(service => service.id === serviceId)
 }
 
--- tests/mocks/userServiceMock.ts
+++ tests/mocks/userServiceMock.ts
@@ -2,6 +2,5 @@ import Interface from './Interface'
 import { UserService } from '../../server/services'
 
 export const userServiceMock = (): Interface<UserService> => ({
-  getUserCaseLoads: jest.fn(),
   getStaffRoles: jest.fn(),
 })
