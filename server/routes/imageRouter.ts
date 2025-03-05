cae0e8f7bc1a80d57b9ba5f32f577a2885e2a91d
Dani Burnley <dan@danops.uk>
2025-03-05 16:35:59 +0000

    CDPS-1244: Create initial photo upload journey

--- server/routes/imageRouter.ts
+++ server/routes/imageRouter.ts
@@ -1,19 +1,29 @@
-import { Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
+import multer from 'multer'
+import dpsComponents from '@ministryofjustice/hmpps-connect-dps-components'
+import MulterFile from '../controllers/interfaces/MulterFile'
 import { Services } from '../services'
 import getPrisonerData from '../middleware/getPrisonerDataMiddleware'
 import auditPageAccessAttempt from '../middleware/auditPageAccessAttempt'
 import { Page } from '../services/auditService'
-import { getRequest } from './routerUtils'
+import { getRequest, postRequest } from './routerUtils'
 import permissionsGuard from '../middleware/permissionsGuard'
 import { mapHeaderData } from '../mappers/headerMappers'
-import { formatName } from '../utils/utils'
+import { formatName, userHasRoles } from '../utils/utils'
 import { NameFormatStyle } from '../data/enums/nameFormatStyle'
 import { formatDateTime } from '../utils/dateHelpers'
 import NotFoundError from '../utils/notFoundError'
+import { PrisonUser } from '../interfaces/HmppsUser'
+import { editProfileEnabled } from '../utils/featureToggles'
+import { HmppsStatusCode } from '../data/enums/hmppsStatusCode'
+import miniBannerData from '../controllers/utils/miniBannerData'
+import config from '../config'
+import logger from '../../logger'
 
 export default function goalsRouter(services: Services): Router {
   const router = Router()
   const get = getRequest(router)
+  const post = postRequest(router)
   const basePath = '/prisoner/:prisonerNumber([a-zA-Z][0-9]{4}[a-zA-Z]{2})'
 
   get(
@@ -97,5 +107,83 @@ export default function goalsRouter(services: Services): Router {
     },
   )
 
+  // REFACTOR THIS
+  const editProfileChecks = () => (req: Request, res: Response, next: NextFunction) => {
+    const { userRoles, activeCaseLoadId } = res.locals.user as PrisonUser
+    if (userHasRoles(['DPS_APPLICATION_DEVELOPER'], userRoles) && editProfileEnabled(activeCaseLoadId)) {
+      return next()
+    }
+    return next(new NotFoundError('User cannot access edit routes', HmppsStatusCode.NOT_FOUND))
+  }
+
+  get(
+    `${basePath}/image/new`,
+    auditPageAccessAttempt({ services, page: Page.Photo }),
+    getPrisonerData(services),
+    permissionsGuard(services.permissionsService.getOverviewPermissions),
+    editProfileChecks(),
+    async (req, res, next) => {
+      const { prisonerData } = req.middleware
+
+      return res.render('pages/edit/photo/addNew', {
+        pageTitle: 'Add a new facial image',
+        miniBannerData: miniBannerData(prisonerData),
+      })
+    },
+  )
+
+  const uploadToBuffer = multer({ storage: multer.memoryStorage() })
+
+  post(
+    `${basePath}/image/new`,
+    auditPageAccessAttempt({ services, page: Page.Photo }),
+    getPrisonerData(services),
+    permissionsGuard(services.permissionsService.getOverviewPermissions),
+    editProfileChecks(),
+    dpsComponents.getPageComponents({
+      logger,
+      includeSharedData: true,
+      dpsUrl: config.serviceUrls.digitalPrison,
+    }),
+    uploadToBuffer.single('photoUpload'),
+    async (req, res, next) => {
+      const { prisonerData } = req.middleware
+      const file = req.file as MulterFile
+
+      res.render('pages/edit/photo/editPhoto', {
+        miniBannerData: miniBannerData(prisonerData),
+        imgSrc: `data:${file.mimetype};base64,${file.buffer.toString('base64')}`,
+        prisonerNumber: req.middleware.prisonerData.prisonerNumber,
+        fileName: file.originalname,
+        fileType: file.mimetype,
+      })
+    },
+  )
+
+  post(
+    `${basePath}/image/submit`,
+    auditPageAccessAttempt({ services, page: Page.Photo }),
+    getPrisonerData(services),
+    permissionsGuard(services.permissionsService.getOverviewPermissions),
+    editProfileChecks(),
+    dpsComponents.getPageComponents({
+      logger,
+      includeSharedData: true,
+      dpsUrl: config.serviceUrls.digitalPrison,
+    }),
+    uploadToBuffer.single('editedFile'),
+    async (req, res, next) => {
+      const { prisonerData } = req.middleware
+      const file = req.file as MulterFile
+
+      res.render('pages/edit/photo/editPhoto', {
+        miniBannerData: miniBannerData(prisonerData),
+        imgSrc: `data:${file.mimetype};base64,${file.buffer.toString('base64')}`,
+        prisonerNumber: req.middleware.prisonerData.prisonerNumber,
+        fileName: file.originalname,
+        fileType: file.mimetype,
+      })
+    },
+  )
   return router
 }
