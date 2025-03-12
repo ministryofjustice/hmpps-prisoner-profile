4bb68f26329aab001029ebeb68c94449dfac79c7
Scott Rowley <16266605+scottrowley@users.noreply.github.com>
2025-03-12 14:59:34 +0000

    CDPS-1289 Amend js script to apply display styles directly

--- assets/js/distinguishingMarkAddImage.js
+++ assets/js/distinguishingMarkAddImage.js
@@ -5,7 +5,6 @@ const inputContainer = document.getElementById('mark-add-photo-upload-control')
 const previewImage = document.getElementById('mark-image-preview')
 const previewFilename = document.getElementById('mark-image-filename')
 const previewContainer = document.getElementById('mark-preview-image-container')
-const hidden = 'govuk-visually-hidden'
 
 function previewSelectedImage() {
   const file = imageInput.files[0]
@@ -16,8 +15,8 @@ function previewSelectedImage() {
       previewImage.src = e.target.result
       previewImage.alt = 'Preview of uploaded image'
       previewFilename.innerText=imageInput.files[0].name
-      previewContainer.classList.remove(hidden)
-      inputContainer.classList.add(hidden)
+      previewContainer.style.display = 'flex'
+      inputContainer.style.display = 'none'
     }
   }
 }
@@ -29,15 +28,7 @@ const changeLink = document.getElementById('change-photo-link')
 function showUploadControls() {
   imageInput.value = null
   previewFilename.innerText=''
-  previewContainer.classList.add(hidden)
-  inputContainer.classList.remove(hidden)
+  previewContainer.style.display = 'none'
+  inputContainer.style.display = 'block'
 }
 changeLink.addEventListener('click', showUploadControls)
-
-// Handle the submit event on the form to correctly redirect based on which button was used to submit
-const form = document.getElementById('add-photo-form')
-
-function handleRedirect(e) {
-  console.log(e.submitter)
-}
-form.addEventListener('submit', handleRedirect)
