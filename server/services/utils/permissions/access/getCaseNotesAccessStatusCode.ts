0050342e64e94796f7b9de15ef2ce449e73650c5
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2025-03-28 15:54:51 +0000

    CDPS-1298: Limit access to case notes

--- server/services/utils/permissions/access/getCaseNotesAccessStatusCode.ts
+++ server/services/utils/permissions/access/getCaseNotesAccessStatusCode.ts
@@ -1,18 +1,34 @@
 import { isInUsersCaseLoad, userHasAllRoles } from '../../../../utils/utils'
 import { HmppsStatusCode } from '../../../../data/enums/hmppsStatusCode'
-import getOverviewAccessStatusCode from './getOverviewAccessStatusCode'
 import Prisoner from '../../../../data/interfaces/prisonerSearchApi/Prisoner'
 import { HmppsUser } from '../../../../interfaces/HmppsUser'
 import { Role } from '../../../../data/enums/role'
 
 export default function getCaseNotesAccessStatusCode(user: HmppsUser, prisoner: Prisoner): HmppsStatusCode {
   const { restrictedPatient } = prisoner
-  const prisonerIsOutOrTransferring = ['OUT', 'TRN'].some(prisonId => prisonId === prisoner.prisonId)
   const prisonerInCaseLoad = isInUsersCaseLoad(prisoner.prisonId, user)
-  const hasGlobalAndPom = userHasAllRoles([Role.GlobalSearch, Role.PomUser], user.userRoles)
+  const hasInactiveBookingsRole = userHasAllRoles([Role.InactiveBookings], user.userRoles)
+  const hasPomRoleInSupportingPrison =
+    userHasAllRoles([Role.PomUser], user.userRoles) && isInUsersCaseLoad(prisoner.supportingPrisonId, user)
 
-  if (!restrictedPatient && !prisonerIsOutOrTransferring && !prisonerInCaseLoad && !hasGlobalAndPom)
-    return HmppsStatusCode.NOT_IN_CASELOAD
+  // Restricted Patient case notes can oly be view by users holding the POM role in the supporting prison or by users
+  // with the inactive bookings role.
+  if (restrictedPatient) {
+    return hasPomRoleInSupportingPrison || hasInactiveBookingsRole
+      ? HmppsStatusCode.OK
+      : HmppsStatusCode.RESTRICTED_PATIENT
+  }
 
-  return getOverviewAccessStatusCode(user, prisoner)
+  // Released prisoner case notes can only be view by users with the inactive bookings role.
+  if (prisoner.prisonId === 'OUT') {
+    return hasInactiveBookingsRole ? HmppsStatusCode.OK : HmppsStatusCode.PRISONER_IS_RELEASED
+  }
+
+  // Transferring prisoner case notes can only be view by users with the inactive bookings role.
+  if (prisoner.prisonId === 'TRN') {
+    return hasInactiveBookingsRole ? HmppsStatusCode.OK : HmppsStatusCode.PRISONER_IS_TRANSFERRING
+  }
+
+  // Active prisoner case notes can only by users assigned to the prisoners caseload.
+  return prisonerInCaseLoad ? HmppsStatusCode.OK : HmppsStatusCode.NOT_IN_CASELOAD
 }
