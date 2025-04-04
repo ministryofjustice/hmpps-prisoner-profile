67e6f27bd632c9dee34ab63806c22ca3b63c7855
David Middleton <david.middleton@digital.justice.gov.uk>
2025-04-04 09:43:04 +0100

    CDPS-944: Update other languages

--- server/controllers/languagesController.ts
+++ server/controllers/languagesController.ts
@@ -5,9 +5,11 @@ import logger from '../../logger'
 import { NameFormatStyle } from '../data/enums/nameFormatStyle'
 import LanguagesService from '../services/languagesService'
 import {
+  LanguagePreferencesDto,
   LanguagePreferencesRequest,
   PersonCommunicationNeedsReferenceDataDomain,
   SecondaryLanguageDto,
+  SecondaryLanguageRequest,
 } from '../data/interfaces/personCommunicationNeedsApi/personCommunicationNeedsApiClient'
 import { PrisonUser } from '../interfaces/HmppsUser'
 import { FlashMessageType } from '../data/enums/flashMessageType'
@@ -109,7 +111,7 @@ export default class LanguagesController {
 
       const { secondaryLanguages } = await this.languagesService.getCommunicationNeeds(clientToken, prisonerNumber)
 
-      this.validate(formValues, secondaryLanguages, errors)
+      this.validateLanguagePreferencesRequest(formValues, secondaryLanguages, errors)
 
       if (errors.length) {
         req.flash('requestBody', JSON.stringify(formValues))
@@ -148,7 +150,146 @@ export default class LanguagesController {
     }
   }
 
-  private validate(
+  public displayUpdateOtherLanguages(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { prisonerName, naturalPrisonerName, prisonerNumber, prisonId, clientToken } =
+        this.getCommonRequestData(req)
+      const { languageCode } = req.params
+      const errors = req.flash('errors')
+      const requestBodyFlash = requestBodyFromFlash<SecondaryLanguageRequest>(req)
+
+      const [communicationNeeds, languageReferenceCodes] = await Promise.all([
+        this.languagesService.getCommunicationNeeds(clientToken, prisonerNumber),
+        this.languagesService.getReferenceData(clientToken, [PersonCommunicationNeedsReferenceDataDomain.language]),
+      ])
+
+      const languageDto =
+        languageCode && communicationNeeds.secondaryLanguages.find(lang => lang.language.code === languageCode)
+
+      const formValues: SecondaryLanguageRequest = requestBodyFlash || {
+        language: languageDto?.language?.code,
+        canRead: languageDto?.canRead,
+        canWrite: languageDto?.canWrite,
+        canSpeak: languageDto?.canSpeak,
+      }
+
+      const languageOptions = [
+        { value: undefined, text: '' },
+        ...objectToSelectOptions(languageReferenceCodes.language ?? [], 'code', 'description', formValues.language),
+      ]
+
+      this.auditService
+        .sendPageView({
+          user: res.locals.user,
+          prisonerNumber,
+          prisonId,
+          correlationId: req.id,
+          page: Page.EditOtherLanguages,
+        })
+        .catch(error => logger.error(error))
+
+      return res.render('pages/languages/updateOtherLanguages', {
+        title: 'Other languages',
+        pageTitle: 'Other languages - Prisoner personal details',
+        languageLabel: `Which other languages does ${naturalPrisonerName} use?`,
+        formValues,
+        errors,
+        miniBannerData: {
+          prisonerNumber,
+          prisonerName,
+        },
+        languageOptions,
+        mainLanguage: communicationNeeds.languagePreferences,
+        otherLanguages: communicationNeeds.secondaryLanguages,
+      })
+    }
+  }
+
+  public submitUpdateOtherLanguages(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { clientToken, prisonerNumber } = this.getCommonRequestData(req)
+      const { languageCode } = req.params
+      const { language, languageSkills, languageError, action } = req.body
+      const formValues: SecondaryLanguageRequest & {
+        languageError?: string
+      } = {
+        language,
+        canSpeak: languageSkills?.includes('canSpeak'),
+        canWrite: languageSkills?.includes('canWrite'),
+        canRead: languageSkills?.includes('canRead'),
+        languageError,
+      }
+
+      const errors = req.errors || []
+
+      const communicationNeeds = await this.languagesService.getCommunicationNeeds(clientToken, prisonerNumber)
+      const secondaryLanguages = communicationNeeds.secondaryLanguages.filter(
+        lang => lang.language.code !== languageCode,
+      )
+
+      this.validateSecondaryLanguageRequest(
+        formValues,
+        communicationNeeds.languagePreferences,
+        secondaryLanguages,
+        errors,
+      )
+
+      if (errors.length) {
+        req.flash('requestBody', JSON.stringify(formValues))
+        req.flash('errors', errors)
+        return res.redirect(
+          `/prisoner/${prisonerNumber}/personal/other-languages${languageCode ? `/${languageCode}` : ''}`,
+        )
+      }
+
+      if (formValues.language) {
+        await this.languagesService.updateOtherLanguage(
+          clientToken,
+          res.locals.user as PrisonUser,
+          prisonerNumber,
+          formValues,
+        )
+        if (languageCode && formValues.language !== languageCode) {
+          await this.languagesService.deleteOtherLanguage(
+            clientToken,
+            res.locals.user as PrisonUser,
+            prisonerNumber,
+            languageCode,
+          )
+        }
+      } else {
+        await this.languagesService.deleteOtherLanguage(
+          clientToken,
+          res.locals.user as PrisonUser,
+          prisonerNumber,
+          languageCode,
+        )
+      }
+
+      req.flash('flashMessage', {
+        text: 'Languages updated',
+        type: FlashMessageType.success,
+        fieldName: 'languages',
+      })
+
+      this.auditService
+        .sendPostSuccess({
+          user: res.locals.user,
+          prisonerNumber,
+          correlationId: req.id,
+          action: PostAction.EditOtherLanguages,
+          details: { formValues },
+        })
+        .catch(error => logger.error(error))
+      return res.redirect(
+        action === 'another'
+          ? `/prisoner/${prisonerNumber}/personal/other-languages`
+          : `/prisoner/${prisonerNumber}/personal#personal-details`,
+      )
+    }
+  }
+
+  private validateLanguagePreferencesRequest(
     formValues: LanguagePreferencesRequest & {
       preferredSpokenLanguageCodeError?: string
       preferredWrittenLanguageCodeError?: string
@@ -182,6 +323,31 @@ export default class LanguagesController {
       errors.push({
         text: 'This is not a valid language',
         href: '#preferredWrittenLanguageCode',
+      })
+    }
+  }
+
+  private validateSecondaryLanguageRequest(
+    formValues: SecondaryLanguageRequest & {
+      languageError?: string
+    },
+    languagePreferences: LanguagePreferencesDto,
+    secondaryLanguages: SecondaryLanguageDto[],
+    errors: HmppsError[],
+  ) {
+    if (
+      languagePreferences.preferredSpokenLanguage?.code === formValues.language ||
+      languagePreferences.preferredWrittenLanguage?.code === formValues.language ||
+      secondaryLanguages.some(lang => lang.language.code === formValues.language)
+    ) {
+      errors.push({
+        text: 'Language must be different from the saved languages',
+        href: '#language',
+      })
+    } else if (formValues.languageError) {
+      errors.push({
+        text: 'This is not a valid language',
+        href: '#language',
       })
     }
   }
