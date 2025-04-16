712cd5d1d9e485b3948ac4fb1775a3387f0dfb5d
Jon Brighton <brightonsbox@hotmail.com>
2025-04-16 09:45:38 +0100

    CDPS-1370: Adding resilience to manage adjudications api calls

--- server/views/dataUtils/adjudicationsSummaryToMiniSummary.test.ts
+++ server/views/dataUtils/adjudicationsSummaryToMiniSummary.test.ts
@@ -1,5 +1,7 @@
 import adjudicationsSummaryToMiniSummary from './adjudicationsSummaryToMiniSummary'
 import AdjudicationsOverviewSummary from '../../services/interfaces/adjudicationsService/AdjudicationsOverviewSummary'
+import { Result } from '../../utils/result/result'
+import { apiErrorMessage } from '../../utils/utils'
 
 describe('adjudicationsSummaryToMiniSummary', () => {
   it('should return a mini summary object', () => {
@@ -8,7 +10,7 @@ describe('adjudicationsSummaryToMiniSummary', () => {
       activePunishments: 2,
     }
     const prisonerNumber = 'A1234BC'
-    const miniSummary = adjudicationsSummaryToMiniSummary(adjudicationsSummary, prisonerNumber)
+    const miniSummary = adjudicationsSummaryToMiniSummary(Result.fulfilled(adjudicationsSummary), prisonerNumber)
     expect(miniSummary).toEqual({
       heading: 'Adjudications',
       topLabel: 'Proven in last 3 months',
@@ -29,7 +31,7 @@ describe('adjudicationsSummaryToMiniSummary', () => {
       activePunishments: 0,
     }
     const prisonerNumber = 'A1234BC'
-    const miniSummary = adjudicationsSummaryToMiniSummary(adjudicationsSummary, prisonerNumber)
+    const miniSummary = adjudicationsSummaryToMiniSummary(Result.fulfilled(adjudicationsSummary), prisonerNumber)
     expect(miniSummary).toEqual({
       heading: 'Adjudications',
       topLabel: 'Proven in last 3 months',
@@ -38,6 +40,23 @@ describe('adjudicationsSummaryToMiniSummary', () => {
       bottomLabel: 'Active',
       bottomContentLine1: 'No active punishments',
       bottomContentLine1Href: undefined,
+      bottomClass: 'small',
+      linkLabel: 'Adjudication history',
+      linkHref: 'http://localhost:3001/adjudication-history/A1234BC',
+    })
+  })
+
+  it('should return a mini summary object with error message if adjudicationSummary is an error', () => {
+    const prisonerNumber = 'A1234BC'
+    const error = {
+      name: 'Error',
+      message: 'Something went wrong!',
+    }
+    const miniSummary = adjudicationsSummaryToMiniSummary(Result.rejected(error), prisonerNumber)
+    expect(miniSummary).toEqual({
+      heading: 'Adjudications',
+      bottomLabel: null,
+      bottomContentLine1: apiErrorMessage,
       bottomClass: 'small',
       linkLabel: 'Adjudication history',
       linkHref: 'http://localhost:3001/adjudication-history/A1234BC',
--- server/views/dataUtils/adjudicationsSummaryToMiniSummary.ts
+++ server/views/dataUtils/adjudicationsSummaryToMiniSummary.ts
@@ -2,20 +2,34 @@ import MiniSummaryData from '../../services/interfaces/overviewPageService/MiniS
 import { pluralise } from '../../utils/pluralise'
 import config from '../../config'
 import AdjudicationsOverviewSummary from '../../services/interfaces/adjudicationsService/AdjudicationsOverviewSummary'
+import { Result } from '../../utils/result/result'
+import { apiErrorMessage } from '../../utils/utils'
 
-export default (adjudicationSummary: AdjudicationsOverviewSummary, prisonerNumber: string): MiniSummaryData => {
+export default (adjudicationSummary: Result<AdjudicationsOverviewSummary>, prisonerNumber: string): MiniSummaryData => {
+  if (adjudicationSummary.status === 'fulfilled') {
+    const { adjudicationCount, activePunishments } = adjudicationSummary.getOrNull()
+    return {
+      heading: 'Adjudications',
+      topLabel: 'Proven in last 3 months',
+      topContent: adjudicationCount,
+      topClass: 'big',
+      bottomLabel: 'Active',
+      bottomContentLine1: pluralise(activePunishments, 'active punishment', {
+        emptyMessage: 'No active punishments',
+      }),
+      bottomContentLine1Href: activePunishments
+        ? `${config.serviceUrls.adjudications}/active-punishments/${prisonerNumber}`
+        : undefined,
+      bottomClass: 'small',
+      linkLabel: 'Adjudication history',
+      linkHref: `${config.serviceUrls.adjudications}/adjudication-history/${prisonerNumber}`,
+    }
+  }
+
   return {
     heading: 'Adjudications',
-    topLabel: 'Proven in last 3 months',
-    topContent: adjudicationSummary.adjudicationCount,
-    topClass: 'big',
-    bottomLabel: 'Active',
-    bottomContentLine1: pluralise(adjudicationSummary.activePunishments, 'active punishment', {
-      emptyMessage: 'No active punishments',
-    }),
-    bottomContentLine1Href: adjudicationSummary.activePunishments
-      ? `${config.serviceUrls.adjudications}/active-punishments/${prisonerNumber}`
-      : undefined,
+    bottomLabel: null,
+    bottomContentLine1: apiErrorMessage,
     bottomClass: 'small',
     linkLabel: 'Adjudication history',
     linkHref: `${config.serviceUrls.adjudications}/adjudication-history/${prisonerNumber}`,
