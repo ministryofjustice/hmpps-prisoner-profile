9fdd322cf4002440c59683d8290127fe527ba9ff
Ewan Donovan <ewan.donovan@digital.justice.gov.uk>
2025-06-26 14:19:18 +0100

    Cleanup

--- server/utils/nomisLockedErrorHelpers.ts
+++ server/utils/nomisLockedErrorHelpers.ts
@@ -10,7 +10,7 @@ export class NomisLockedError extends Error {
 export async function handleNomisLockedError<T>(apiClientFunction: () => Promise<T>): Promise<T> {
   try {
     return await apiClientFunction()
-  } catch (e: any) {
+  } catch (e) {
     if (e?.status === 423) {
       throw new NomisLockedError(e.message)
     }
