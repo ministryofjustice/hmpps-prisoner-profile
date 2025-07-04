8b43bfad34bb1d91aedeb2ab4b6caae6fb06d370
Jon Brighton <brightonsbox@hotmail.com>
2025-07-04 14:26:17 +0100

    CDPS-1460: Using profile edit permissions

--- server/routes/languagesRouter.ts
+++ server/routes/languagesRouter.ts
@@ -1,23 +1,23 @@
-import { RequestHandler, Router } from 'express'
+import { Router } from 'express'
 import { getRequest, postRequest } from './routerUtils'
 import { Services } from '../services'
 import LanguagesController from '../controllers/languagesController'
 
-export default function languagesRouter(services: Services, editProfileChecks: () => RequestHandler): Router {
+export default function languagesRouter(services: Services): Router {
   const router = Router({ mergeParams: true })
   const get = getRequest(router)
   const post = postRequest(router)
 
   const languagesController = new LanguagesController(services.languagesService, services.auditService)
 
-  get('/main-language', editProfileChecks(), languagesController.displayUpdateMainLanguage())
-  post('/main-language', editProfileChecks(), languagesController.submitUpdateMainLanguage())
+  get('/main-language', languagesController.displayUpdateMainLanguage())
+  post('/main-language', languagesController.submitUpdateMainLanguage())
 
-  get('/other-languages', editProfileChecks(), languagesController.displayUpdateOtherLanguages())
-  post('/other-languages', editProfileChecks(), languagesController.submitUpdateOtherLanguages())
+  get('/other-languages', languagesController.displayUpdateOtherLanguages())
+  post('/other-languages', languagesController.submitUpdateOtherLanguages())
 
-  get('/other-languages/:languageCode', editProfileChecks(), languagesController.displayUpdateOtherLanguages())
-  post('/other-languages/:languageCode', editProfileChecks(), languagesController.submitUpdateOtherLanguages())
+  get('/other-languages/:languageCode', languagesController.displayUpdateOtherLanguages())
+  post('/other-languages/:languageCode', languagesController.submitUpdateOtherLanguages())
 
   return router
 }
