fde9126efad3ce62c28d37699a2ed7859ed48ac3
Melia Burnley <melia@pintsize.it>
2025-07-09 15:55:28 +0100

    CDPS-1382: Display alias name change reason error correctly
    (cherry picked from commit 33c5658ee9dc72cae5d627a96ad7fac5bf82f2ef)

--- server/controllers/aliasController.test.ts
+++ server/controllers/aliasController.test.ts
@@ -103,7 +103,7 @@ describe('Alias Controller', () => {
 
       if (!purpose) {
         expect(req.flash).toHaveBeenCalledWith('errors', [
-          { text: `Select why you’re changing John Saunders’ name`, href: '#radio' },
+          { text: `Select why you’re changing John Saunders’ name`, href: '#radioField' },
         ])
       } else {
         expect(auditService.sendPostSuccess).toHaveBeenCalledWith({
