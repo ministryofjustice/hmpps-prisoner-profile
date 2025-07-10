4265a23469faab76dc9966676d2342feb3bbf781
Scott Rowley <16266605+scottrowley@users.noreply.github.com>
2025-07-10 17:30:48 +0100

    CDPS-1511 Ignore autocomplete when another option is selected (#1111)

--- assets/js/radioFieldWithAutocomplete.js
+++ assets/js/radioFieldWithAutocomplete.js
@@ -5,23 +5,24 @@ accessibleAutocomplete.enhanceSelectElement({
   menuClasses: 'govuk-input--width-30',
 })
 
-$(function() {
-  $('.js-autocomplete-submit').on('click', function() {
+$(function () {
+  $('.js-autocomplete-submit').on('click', function () {
+    const selectedOption = $('input[name=radioField]:checked')
     const autocompleteInput = $('.autocomplete__input').val()
 
-    if (!$.trim(autocompleteInput).length) {
+    if (selectedOption.val() !== 'OTHER' || !$.trim(autocompleteInput).length) {
       $('.js-autocomplete-select').val('')
       return
     }
 
     const options = $('#autocomplete-select option')
-    const textValues = $.map(options, function(option) {
+    const textValues = $.map(options, function (option) {
       return option.text
     })
 
     if (!textValues.includes(autocompleteInput)) {
       $('.js-autocomplete-select').val('')
-      $('input[name=radioField]:checked').val('OTHER__VALIDATION_ERROR')
+      selectedOption.val('OTHER__VALIDATION_ERROR')
     }
   })
 })
