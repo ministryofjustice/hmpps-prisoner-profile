a11a94496348983981fc57c177485efd0eaaa343
Jon Brighton <brightonsbox@hotmail.com>
2025-07-14 15:52:11 +0100

    Inc3449195 revert (#1129)
    * Revert "Fixing linting (#1127)"
    
    This reverts commit 8212779b260ad0d6425d9be9c6105907ab09fcf2.
    
    * Revert "Futher belief disable (#1126)"
    
    This reverts commit 9ebc3966bc6d3cf7fd361737f91015355945f897.
    
    * Revert "inc3449195 (#1125)"
    
    This reverts commit ad3741ebc31bd14146e53e85891861492195c84a.
    
    * Reestablishing BVLS_FEATURE_HMCTS_LINK_GUEST_PIN=false in prod
    
    * Temporarily removing integration tests for speed of deploy

---
+++ server/controllers/beliefHistoryController.test.ts
@@ -0,0 +1,82 @@
+import { PrisonerMockDataA } from '../data/localMockData/prisoner'
+import { inmateDetailMock } from '../data/localMockData/inmateDetailMock'
+import BeliefHistoryController from './beliefHistoryController'
+import BeliefService from '../services/beliefService'
+import { beliefHistoryMock, beliefHistoryOverrideMock } from '../data/localMockData/beliefHistoryMock'
+import { auditServiceMock } from '../../tests/mocks/auditServiceMock'
+import { Role } from '../data/enums/role'
+import { CaseLoadsDummyDataA } from '../data/localMockData/caseLoad'
+
+describe('Prisoner belief history', () => {
+  const prisonerNumber = 'G6123VU'
+
+  let req: any
+  let res: any
+  let controller: BeliefHistoryController
+
+  beforeEach(() => {
+    req = {
+      middleware: {
+        clientToken: 'CLIENT_TOKEN',
+        prisonerData: PrisonerMockDataA,
+        inmateDetail: inmateDetailMock,
+      },
+      originalUrl: 'http://localhost',
+      params: { prisonerNumber },
+      query: {},
+      protocol: 'http',
+      get: jest.fn().mockReturnValue('localhost'),
+    }
+    res = {
+      locals: {
+        user: {
+          activeCaseLoadId: 'MDI',
+          userRoles: [Role.PrisonUser],
+          caseLoads: CaseLoadsDummyDataA,
+          token: 'TOKEN',
+        },
+      },
+      render: jest.fn(),
+      status: jest.fn(),
+    }
+    controller = new BeliefHistoryController(new BeliefService(null), auditServiceMock())
+  })
+
+  afterEach(() => {
+    const spy = jest.spyOn(Date, 'now')
+    spy.mockRestore()
+  })
+
+  describe('displayBeliefHistory', () => {
+    it('should call the service and render the page', async () => {
+      jest.spyOn<any, string>(controller['beliefService'], 'getBeliefHistory').mockResolvedValue(beliefHistoryMock)
+
+      await controller.displayBeliefHistory(req, res)
+      expect(res.render).toHaveBeenCalledWith('pages/beliefHistory', {
+        pageTitle: 'Religion, faith or belief history',
+        pageHeading: `John Saunders’ religion, faith or belief history`,
+        beliefs: beliefHistoryMock,
+        prisonerNumber,
+        breadcrumbPrisonerName: 'Saunders, John',
+      })
+    })
+
+    it('should use religion field date override values to render the belief descriptions', async () => {
+      jest
+        .spyOn<any, string>(controller['beliefService'], 'getBeliefHistory')
+        .mockResolvedValue(beliefHistoryOverrideMock)
+
+      await controller.displayBeliefHistory(req, res)
+      expect(res.render).toHaveBeenCalledWith('pages/beliefHistory', {
+        pageTitle: 'Religion, faith or belief history',
+        pageHeading: `John Saunders’ religion, faith or belief history`,
+        beliefs: [
+          { ...beliefHistoryOverrideMock[0], beliefDescription: 'Other religion, faith or belief' },
+          beliefHistoryOverrideMock[1],
+        ],
+        prisonerNumber,
+        breadcrumbPrisonerName: 'Saunders, John',
+      })
+    })
+  })
+})
