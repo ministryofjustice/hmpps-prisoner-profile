8212779b260ad0d6425d9be9c6105907ab09fcf2
Jon Brighton <brightonsbox@hotmail.com>
2025-07-14 14:19:42 +0100

    Fixing linting (#1127)

--- server/services/personalPageService.ts
+++ server/services/personalPageService.ts
@@ -330,7 +330,7 @@ export default class PersonalPageService {
         ),
       },
       learnerNeurodivergence,
-      hasCurrentBelief: false,
+      hasCurrentBelief: beliefs != null,
       distinguishingMarks,
       militaryRecords: militaryRecords?.filter(record => record.militarySeq === 1), // Temporary fix to only show the first military record - designs for multiple not ready yet
       globalNumbersAndEmails,
