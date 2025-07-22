8056ce59d5b3aadc884af186a80ebf8db4abca84
Michael Willis <michael.willis2@justice.gov.uk>
2025-07-22 13:23:18 +0100

    INC-1396 Refactor incentives service to use prisonerNumber instead of bookingId (#1153)
    * INC-1396 Refactor incentives service to use prisonerNumber instead of bookingId
    
    * Update `stubGetReviews` to include `bookingId` parameter

--- server/data/interfaces/incentivesApi/incentivesApiClient.ts
+++ server/data/interfaces/incentivesApi/incentivesApiClient.ts
@@ -1,5 +1,5 @@
 import IncentiveReviews from './IncentiveReviews'
 
 export interface IncentivesApiClient {
-  getReviews(bookingId: number): Promise<IncentiveReviews>
+  getReviews(prisonerNumber: string): Promise<IncentiveReviews>
 }
