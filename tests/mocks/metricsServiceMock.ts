985c983c9dad9d6e00d5def019ff830929f7ae74
Melia Burnley <melia.burnley@digital.justice.gov.uk>
2025-07-31 11:03:52 +0100

    CDPS-1573: Create metrics event on updating the profile image (#1170)

---
+++ tests/mocks/metricsServiceMock.ts
@@ -0,0 +1,10 @@
+import MetricsService from '../../server/services/metrics/metricsService'
+
+export const metricsServiceMock = (): MetricsService => ({
+  trackPrisonPersonUpdate: jest.fn(),
+  trackPersonIntegrationUpdate: jest.fn(),
+  trackHealthAndMedicationUpdate: jest.fn(),
+  trackPersonCommunicationNeedsUpdate: jest.fn(),
+  trackPersonalRelationshipsUpdate: jest.fn(),
+  trackNomisLockedWarning: jest.fn(),
+})
