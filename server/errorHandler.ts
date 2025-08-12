8323e8f4ed0f628eff65ab81fbfceb577897ae37
Melia Burnley <melia@pintsize.it>
2025-08-12 14:20:07 +0100

    CDPS-1561: Handle new response error codes

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -1,14 +1,30 @@
 import type { NextFunction, Request, Response } from 'express'
 import type { HTTPError } from 'superagent'
+import { SanitisedError } from '@ministryofjustice/hmpps-rest-client'
 import logger from '../logger'
 import NotFoundError from './utils/notFoundError'
 import ServerError from './utils/serverError'
 import RoleError from './utils/roleError'
 import { HmppsStatusCode } from './data/enums/hmppsStatusCode'
 
+/*
+  The errors we get sometimes have the status in .status but also can the status on .responseStatus.
+  As a result we need to handle both in the same way as an "any" type to prevent type errors where 
+  there's no overlap.
+*/
 export default function createErrorHandler(production: boolean) {
+  const getStatus = (error: any): number | undefined => {
+    if (error.status) return error.status
+    if (error.responseStatus) return error.responseStatus
+    return undefined
+  }
+
+  const hasStatus = (error: any, status: number): boolean => {
+    return (error.status && error.status === status) || (error.responseStatus && error.responseStatus === status)
+  }
+
   return (
-    error: HTTPError | NotFoundError | ServerError | RoleError,
+    error: HTTPError | NotFoundError | ServerError | RoleError | SanitisedError,
     req: Request,
     res: Response,
     next: NextFunction,
@@ -19,25 +35,25 @@ export default function createErrorHandler(production: boolean) {
       logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
     }
 
-    if (error.status === 401) {
+    if (hasStatus(error, 401)) {
       logger.info('Logging user out')
       return res.redirect('/sign-out')
     }
 
     res.locals.hideBackLink = true
 
-    if (error.status === 404 || error.status === 403) {
-      res.status(error.status)
+    if (hasStatus(error, 404) || hasStatus(error, 403)) {
+      res.status(getStatus(error))
       return res.render('notFound', { url: req.headers.referer || '/' })
     }
 
     res.locals.message = production
       ? 'Something went wrong. The error has been logged. Please try again'
       : error.message
-    res.locals.status = error.status
+    res.locals.status = getStatus(error)
     res.locals.stack = production ? null : error.stack
 
-    res.status(error.status || 500)
+    res.status(getStatus(error) || 500)
 
     return res.render('pages/error')
   }
