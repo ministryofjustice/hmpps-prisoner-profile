2cb77f24ca28d28d10186ff216efd6a88c79e1a2
Melia Burnley <melia@pintsize.it>
2025-08-13 09:31:32 +0100

    CDPS-1561: Extract error helpers to utils

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -6,23 +6,9 @@ import NotFoundError from './utils/notFoundError'
 import ServerError from './utils/serverError'
 import RoleError from './utils/roleError'
 import { HmppsStatusCode } from './data/enums/hmppsStatusCode'
+import { errorHasStatus, getErrorStatus } from './utils/errorHelpers'
 
-/*
-  The errors we get sometimes have the status in .status but also can the status on .responseStatus.
-  As a result we need to handle both in the same way as an "any" type to prevent type errors where 
-  there's no overlap.
-*/
 export default function createErrorHandler(production: boolean) {
-  const getStatus = (error: any): number | undefined => {
-    if (error.status) return error.status
-    if (error.responseStatus) return error.responseStatus
-    return undefined
-  }
-
-  const hasStatus = (error: any, status: number): boolean => {
-    return (error.status && error.status === status) || (error.responseStatus && error.responseStatus === status)
-  }
-
   return (
     error: HTTPError | NotFoundError | ServerError | RoleError | SanitisedError,
     req: Request,
@@ -35,25 +21,25 @@ export default function createErrorHandler(production: boolean) {
       logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
     }
 
-    if (hasStatus(error, 401)) {
+    if (errorHasStatus(error, 401)) {
       logger.info('Logging user out')
       return res.redirect('/sign-out')
     }
 
     res.locals.hideBackLink = true
 
-    if (hasStatus(error, 404) || hasStatus(error, 403)) {
-      res.status(getStatus(error))
+    if (errorHasStatus(error, 404) || errorHasStatus(error, 403)) {
+      res.status(getErrorStatus(error))
       return res.render('notFound', { url: req.headers.referer || '/' })
     }
 
     res.locals.message = production
       ? 'Something went wrong. The error has been logged. Please try again'
       : error.message
-    res.locals.status = getStatus(error)
+    res.locals.status = getErrorStatus(error)
     res.locals.stack = production ? null : error.stack
 
-    res.status(getStatus(error) || 500)
+    res.status(getErrorStatus(error) || 500)
 
     return res.render('pages/error')
   }
