6e710d837769d5a2ec58d67cd57d0668c227af48
Scott Rowley <16266605+scottrowley@users.noreply.github.com>
2025-10-24 16:46:55 +0100

    CDPS-1624 Align 'Add case note' button with permissions library

--- server/utils/roleHelpers.ts
+++ server/utils/roleHelpers.ts
@@ -3,10 +3,7 @@ import { userCanEdit, userHasAllRoles } from './utils'
 import Prisoner from '../data/interfaces/prisonerSearchApi/Prisoner'
 import { HmppsUser } from '../interfaces/HmppsUser'
 
+// eslint-disable-next-line import/prefer-default-export
 export const canViewCaseNotes = (user: HmppsUser, prisoner: Partial<Prisoner>) => {
-  return userHasAllRoles([Role.GlobalSearch, Role.PomUser], user.userRoles) || userCanEdit(user, prisoner)
-}
-
-export const canAddCaseNotes = (user: HmppsUser, prisoner: Partial<Prisoner>) => {
   return userHasAllRoles([Role.GlobalSearch, Role.PomUser], user.userRoles) || userCanEdit(user, prisoner)
 }
