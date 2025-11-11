f274c3811c3477ea2a143db513a07ca71c013b03
Jon Brighton <brightonsbox@hotmail.com>
2025-11-11 11:44:22 +0000

    Revert "CDPS-1688: Move to Node 24 LTS (#1328)"
    This reverts commit b9781eafa3b29bdd0157a063a04061ac9fba3c64.

--- esbuild/assets.config.js
+++ esbuild/assets.config.js
@@ -1,54 +1,65 @@
 const { globSync } = require('node:fs')
 const path = require('node:path')
+
 const { copy } = require('esbuild-plugin-copy')
 const { sassPlugin } = require('esbuild-sass-plugin')
 const { clean } = require('esbuild-plugin-clean')
 const manifestPlugin = require('esbuild-plugin-manifest')
-const { buildNotificationPlugin } = require('./utils')
+const esbuild = require('esbuild')
 
 /**
  * Copy additional assets into distribution
  * @type {BuildStep}
  */
-const getAdditionalAssetsConfig = buildConfig => ({
-  outdir: buildConfig.assets.outDir,
-  plugins: [
-    copy({
-      resolveFrom: 'cwd',
-      assets: buildConfig.assets.copy,
-    }),
-    buildNotificationPlugin('Assets (Additional)', buildConfig.isWatchMode),
-  ],
-})
+const buildAdditionalAssets = buildConfig => {
+  return esbuild.build({
+    outdir: buildConfig.assets.outDir,
+    plugins: [
+      copy({
+        resolveFrom: 'cwd',
+        assets: buildConfig.assets.copy,
+      }),
+    ],
+  })
+}
 
 /**
  * Build scss and javascript assets
  * @type {BuildStep}
  */
-const getAssetsConfig = buildConfig => ({
-  entryPoints: buildConfig.assets.entryPoints,
-  outdir: buildConfig.assets.outDir,
-  entryNames: '[ext]/[name].[hash]',
-  minify: buildConfig.isProduction,
-  sourcemap: !buildConfig.isProduction,
-  platform: 'browser',
-  target: 'es2018',
-  external: ['/assets/*'],
-  bundle: true,
-  plugins: [
-    clean({
-      patterns: globSync(buildConfig.assets.clear),
-    }),
-    manifestPlugin({
-      generate: entries =>
-        Object.fromEntries(Object.entries(entries).map(paths => paths.map(p => p.replace(/^dist\//, '/')))),
-    }),
-    sassPlugin({
-      quietDeps: true,
-      loadPaths: [process.cwd(), path.join(process.cwd(), 'node_modules')],
-    }),
-    buildNotificationPlugin('Assets', buildConfig.isWatchMode),
-  ],
-})
+const buildAssets = buildConfig => {
+  return esbuild.build({
+    entryPoints: buildConfig.assets.entryPoints,
+    outdir: buildConfig.assets.outDir,
+    entryNames: '[ext]/[name].[hash]',
+    minify: buildConfig.isProduction,
+    sourcemap: !buildConfig.isProduction,
+    platform: 'browser',
+    target: 'es2018',
+    external: ['/assets/*'],
+    bundle: true,
+    plugins: [
+      clean({
+        patterns: globSync(buildConfig.assets.clear),
+      }),
+      manifestPlugin({
+        generate: entries =>
+          Object.fromEntries(Object.entries(entries).map(paths => paths.map(p => p.replace(/^dist\//, '/')))),
+      }),
+      sassPlugin({
+        quietDeps: true,
+        loadPaths: [process.cwd(), path.join(process.cwd(), 'node_modules')],
+      }),
+    ],
+  })
+}
 
-module.exports = { getAssetsConfig, getAdditionalAssetsConfig }
+/**
+ * @param {BuildConfig} buildConfig
+ * @returns {Promise}
+ */
+module.exports = buildConfig => {
+  process.stderr.write('\u{1b}[1m\u{2728} Building assets...\u{1b}[0m\n')
+
+  return Promise.all([buildAssets(buildConfig), buildAdditionalAssets(buildConfig)])
+}
