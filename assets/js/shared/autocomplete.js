deb6f0dd4faecdc6db32cc907aade035e40de937
Ewan Donovan <ewan.donovan@digital.justice.gov.uk>
2025-12-10 14:32:58 +0000

    Add simple sorting

--- assets/js/shared/autocomplete.js
+++ assets/js/shared/autocomplete.js
@@ -1,18 +1,44 @@
 import accessibleAutocomplete from 'accessible-autocomplete'
 import { getParentConditionalRadioInput } from './helpers'
 
+function filterAndSort(options) {
+  const scoreBySubstringIndex = (query, option) => {
+    const index = option.toLowerCase().indexOf(query.toLowerCase())
+    return index === -1 ? Infinity : index
+  }
+
+  return (query, populateResults) => {
+    const lowerCasedQuery = query.trim().toLowerCase()
+
+    if (!lowerCasedQuery) {
+      populateResults(options)
+      return
+    }
+
+    const filteredOptions = options.filter(option => option.toLowerCase().includes(lowerCasedQuery))
+
+    const sortedOptions = filteredOptions.sort((left, right) => {
+      return scoreBySubstringIndex(query, left) - scoreBySubstringIndex(query, right)
+    })
+
+    populateResults(sortedOptions)
+  }
+}
+
 export function autocomplete() {
   document.querySelectorAll('.js-autocomplete-select').forEach(selectElement => {
     const invalidInput = document.getElementById(`${selectElement.id}-error`)?.value
 
     const inputClasses = selectElement.getAttribute('data-input-classes')
     const menuClasses = selectElement.getAttribute('data-menu-classes')
+    const options = Array.from(selectElement.querySelectorAll('option')).map(option => option.textContent || '')
 
     accessibleAutocomplete.enhanceSelectElement({
       defaultValue: invalidInput ?? '',
       selectElement,
       inputClasses,
       menuClasses,
+      source: filterAndSort(options),
     })
   })
 
@@ -32,11 +58,9 @@ export function autocomplete() {
         }
 
         const options = $(`#${inputElement.id}-select`).find('option')
-        const textValues = $.map(options, function (option) {
-          return option.text
-        })
+        const textValues = $.map(options, option => option.text)
 
-        if (!textValues.map(value => value.toLowerCase()).includes(autocompleteInput.toLowerCase())) {
+        if (!textValues.map(v => v.toLowerCase()).includes(autocompleteInput.toLowerCase())) {
           autocompleteSelect.value = ''
           errorField.value = autocompleteInput
         } else {
