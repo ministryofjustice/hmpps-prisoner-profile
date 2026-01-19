dc681844d15d16259dab3b18b320ba28fa82f1b6
Igor <igor.ushkarev1@justice.gov.uk>
2026-01-19 16:54:24 +0000

    Update dependencies

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -5,7 +5,7 @@ export default configureAllowedScripts({
     // Needed by esbuild for watching files during development
     'node_modules/@parcel/watcher@2.5.4': 'ALLOW',
     // Needed for running integration tests:
-    'node_modules/cypress@15.8.2': 'ALLOW',
+    'node_modules/cypress@15.9.0': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
