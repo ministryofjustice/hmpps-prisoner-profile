9e4f7e9b576b006f9f7604e55b9102bbb930ad69
Igor <igor.ushkarev1@justice.gov.uk>
2026-02-06 15:16:53 +0000

    Remove outdated client-side polyfill for `NodeList.forEach` – it’s now “baseline” and available since 2017

--- assets/js/shared/helpers.ts
+++ assets/js/shared/helpers.ts
@@ -1,15 +1,3 @@
-export function nodeListForEach<E extends Element>(
-  nodes: NodeListOf<E>,
-  callback: (this: Window, node: E, index: number, nodes: NodeListOf<E>) => void,
-) {
-  if (window.NodeList.prototype.forEach) {
-    return nodes.forEach(callback)
-  }
-  for (let i = 0; i < nodes.length; i++) {
-    callback.call(window, nodes[i], i, nodes)
-  }
-}
-
 export function getParentConditionalRadioInput(element: Element): HTMLInputElement | undefined {
   return element?.closest('.govuk-radios__conditional')?.previousElementSibling?.querySelector('input')
 }
