3381f3c683ab3bed6610c0c7b3d32b9aacadb690
Nick Johnson <136325256+nj963@users.noreply.github.com>
2026-04-09 12:21:37 +0100

    CDPS-1858: Refactor 5/14 - physicalCharacteristicsController.ts (#1619)
    * CDPS-1858: Refactor 5/15 - physicalCharacteristicsController.ts
    
    * linting grrr

--- server/controllers/personal/edit/personalEditControllerFactory.ts
+++ server/controllers/personal/edit/personalEditControllerFactory.ts
@@ -3,6 +3,7 @@ import { AuditService } from '../../../services/auditService'
 import CityOrTownOfBirthController from './cityOrTownOfBirth/cityOrTownOfBirthController'
 import HeightController from './height/heightController'
 import NationalityController from './nationality/nationalityController'
+import PhysicalCharacteristicsController from './physicalCharacteristics/physicalCharacteristicsController'
 import ShoeSizeController from './shoeSize/shoeSizeController'
 import SmokerOrVaperController from './smokerOrVaper/smokerOrVaperController'
 import WeightController from './weight/weightController'
@@ -15,6 +16,7 @@ export default function personalEditControllerFactory(
     cityOrTownOfBirthController: new CityOrTownOfBirthController(personalPageService, auditService),
     heightController: new HeightController(personalPageService, auditService),
     nationalityController: new NationalityController(personalPageService, auditService),
+    physicalCharacteristicsController: new PhysicalCharacteristicsController(personalPageService, auditService),
     shoeSizeController: new ShoeSizeController(personalPageService, auditService),
     smokerOrVaperController: new SmokerOrVaperController(personalPageService, auditService),
     weightController: new WeightController(personalPageService, auditService),
