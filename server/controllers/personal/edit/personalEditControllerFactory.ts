242af6f2b5795497410c08940ce71c18401e014e
Nick Johnson <136325256+nj963@users.noreply.github.com>
2026-04-09 14:05:56 +0100

    CDPS-1858: Refactor 6/14 - eyeColourController.ts (#1620)
    * CDPS-1858: Refactor 6/15 - eyeColourController.ts
    
    * linting grrr

--- server/controllers/personal/edit/personalEditControllerFactory.ts
+++ server/controllers/personal/edit/personalEditControllerFactory.ts
@@ -1,6 +1,7 @@
 import PersonalPageService from '../../../services/personalPageService'
 import { AuditService } from '../../../services/auditService'
 import CityOrTownOfBirthController from './cityOrTownOfBirth/cityOrTownOfBirthController'
+import EyeColourController from './eyeColour/eyeColourController'
 import HeightController from './height/heightController'
 import NationalityController from './nationality/nationalityController'
 import PhysicalCharacteristicsController from './physicalCharacteristics/physicalCharacteristicsController'
@@ -14,6 +15,7 @@ export default function personalEditControllerFactory(
 ) {
   return {
     cityOrTownOfBirthController: new CityOrTownOfBirthController(personalPageService, auditService),
+    eyeColourController: new EyeColourController(personalPageService, auditService),
     heightController: new HeightController(personalPageService, auditService),
     nationalityController: new NationalityController(personalPageService, auditService),
     physicalCharacteristicsController: new PhysicalCharacteristicsController(personalPageService, auditService),
