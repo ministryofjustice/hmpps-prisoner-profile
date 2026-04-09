801d2c1b4f48a643f06330e4c594eacf07156575
Nick Johnson <136325256+nj963@users.noreply.github.com>
2026-04-09 10:35:03 +0100

    CDPS-1858: Refactor 1/14 - weightController.ts (#1610)
    CDPS-1858: Refactor 1/15 - weightController.ts

--- server/controllers/personal/edit/personalEditControllerFactory.ts
+++ server/controllers/personal/edit/personalEditControllerFactory.ts
@@ -2,6 +2,7 @@ import PersonalPageService from '../../../services/personalPageService'
 import { AuditService } from '../../../services/auditService'
 import ShoeSizeController from './shoeSize/shoeSizeController'
 import HeightController from './height/heightController'
+import WeightController from './weight/weightController'
 
 export default function personalEditControllerFactory(
   personalPageService: PersonalPageService,
@@ -10,5 +11,6 @@ export default function personalEditControllerFactory(
   return {
     heightController: new HeightController(personalPageService, auditService),
     shoeSizeController: new ShoeSizeController(personalPageService, auditService),
+    weightController: new WeightController(personalPageService, auditService),
   }
 }
