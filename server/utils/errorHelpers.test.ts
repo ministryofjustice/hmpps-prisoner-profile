46018704a6c59b446c336cadbaa72ff76baa901c
Igor <igor.ushkarev1@justice.gov.uk>
2026-04-16 12:47:16 +0100

    Allow error status helpers to work on any type

--- server/utils/errorHelpers.test.ts
+++ server/utils/errorHelpers.test.ts
@@ -1,3 +1,5 @@
+import NotFoundError from './notFoundError'
+import ServerError from './serverError'
 import { errorHasStatus, getErrorStatus } from './errorHelpers'
 
 describe('Error helpers', () => {
@@ -15,7 +17,12 @@ describe('Error helpers', () => {
     it.each([
       ['Error with status', errorWithStatus, 404],
       ['Error with responseStatus', errorWithResponseStatus, 500],
+      ['NotFoundError', new NotFoundError(), 404],
+      ['ServerError', new ServerError(), 500],
       ['Error with neither', errorWithNeither, undefined],
+      ['Generic Error', new Error('problem'), undefined],
+      ['undefined', undefined, undefined],
+      ['null', null, undefined],
     ])('Gets the correct error code: %s', (_, err, expected) => {
       expect(getErrorStatus(err)).toEqual(expected)
     })
@@ -27,7 +34,12 @@ describe('Error helpers', () => {
       ['Error with status: false', errorWithStatus, 500, false],
       ['Error with responseStatus: true', errorWithResponseStatus, 500, true],
       ['Error with responseStatus: false', errorWithResponseStatus, 404, false],
+      ['NotFoundError has status 404', new NotFoundError(), 404, true],
+      ['ServerError does not have status 404', new ServerError(), 404, false],
       ['Error with neither', errorWithNeither, 400, false],
+      ['Generic Error', new Error('problem'), 400, false],
+      ['undefined', undefined, 400, false],
+      ['null', null, 400, false],
     ])('Gets the correct error code: %s', (_, err, status, expected) => {
       expect(errorHasStatus(err, status)).toEqual(expected)
     })
--- server/utils/errorHelpers.ts
+++ server/utils/errorHelpers.ts
@@ -1,18 +1,20 @@
-/*
-  The errors we get sometimes have the status in .status but also can the status on .responseStatus.
-  As a result we need to handle both in the same way as an "any" type to prevent type errors where
-  there's no overlap.
-*/
-export const getErrorStatus = (error: object): number | undefined => {
-  if ('status' in error && typeof error.status === 'number') {
-    return error.status
-  }
-  if ('responseStatus' in error && typeof error.responseStatus === 'number') {
-    return error.responseStatus
+/**
+ * The errors we get sometimes have the status in .status but also can the status on .responseStatus.
+ * As a result we need to handle both in the same way as an "any" type to prevent type errors where
+ * there's no overlap.
+ */
+export const getErrorStatus = (error: unknown): number | undefined => {
+  if (typeof error === 'object' && error !== null) {
+    if ('status' in error && typeof error.status === 'number') {
+      return error.status
+    }
+    if ('responseStatus' in error && typeof error.responseStatus === 'number') {
+      return error.responseStatus
+    }
   }
   return undefined
 }
 
-export const errorHasStatus = (error: object, status: number): boolean => {
+export const errorHasStatus = (error: unknown, status: number): boolean => {
   return getErrorStatus(error) === status
 }
