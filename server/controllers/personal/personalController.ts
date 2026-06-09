83ebfc5dfc44b1399f8d95e7a78f18aa94b96538
Nick Johnson <136325256+nj963@users.noreply.github.com>
2026-06-09 11:44:10 +0100

    CDPS-1984 Add yellow banner to prisoner profile for edit-the-profile functionality (#1720)
    * CDPS-1984 Add yellow banner to prisoner profile for edit-the-profile functionality
    
    * CDPS-1984 Update links
    
    * CDPS-1984 a -> a:link

--- server/controllers/personal/personalController.ts
+++ server/controllers/personal/personalController.ts
@@ -72,6 +72,7 @@ export default class PersonalController {
         hasPersonalId,
         hasHomeOfficeId,
         useCustomErrorBanner: true,
+        showEditProfileBanner: editEnabled,
         changeContactLinkEnabled,
       })
     }
