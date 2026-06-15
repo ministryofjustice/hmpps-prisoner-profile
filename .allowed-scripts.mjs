15c46eea6759c047b728114ce7bad7f355ccf126
Ewan-Donovan <ewan.donovan@digital.justice.gov.uk>
2026-06-15 15:55:55 +0100

    allowlist

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -9,7 +9,7 @@ export default configureAllowedScripts({
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.28.0': 'ALLOW',
+    'node_modules/esbuild@0.28.1': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
