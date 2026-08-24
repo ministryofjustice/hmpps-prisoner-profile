d4fa2c5683dcd3addbad237a9799c7cece63d320
Igor <igor.ushkarev1@justice.gov.uk>
2026-08-24 15:12:54 +0100

    Move modal component to DPS shared items package

--- assets/js/shared/alertFlags.ts
+++ assets/js/shared/alertFlags.ts
@@ -1,11 +1,8 @@
-import { Modal } from './modal'
+import { Modal } from '@ministryofjustice/hmpps-connect-dps-shared-items/dist/assets/dps/components/modal'
 
 export function alertFlags() {
-  const alertDetailsElement = document.getElementById('alert-details')
-
-  if (alertDetailsElement) {
-    const alertDetailsModal = new Modal(alertDetailsElement)
-
+  const alertDetailsModal = Modal.getById('alert-details')
+  if (alertDetailsModal) {
     document.querySelectorAll<HTMLAnchorElement>('.alerts-list .dps-alert-status').forEach(el => {
       el.addEventListener('click', event => {
         event.preventDefault()
