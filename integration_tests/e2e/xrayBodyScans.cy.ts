e245f22f2a71a6d9abd1748b0b6907cf8cec82d0
Igor <igor.ushkarev1@justice.gov.uk>
2026-09-23 10:37:22 +0100

    Update design of old xray scan history page

--- integration_tests/e2e/xrayBodyScans.cy.ts
+++ integration_tests/e2e/xrayBodyScans.cy.ts
@@ -91,7 +91,7 @@ context('X-ray body scans', () => {
     cy.signIn({ redirectPath: `prisoner/${prisonerNumber}/x-ray-body-scans` })
     const page = Page.verifyOnPageWithTitle(XrayBodyScans, possessivePrisonerName)
     page.mixedScansNote.should('not.exist')
-    page.alert.should('contain.text', 'The scan history could not be loaded')
+    page.alert.should('contain.text', 'There is a problem loading the scan history')
     page.bodyScansTable.should('not.exist')
   })
 
